9c75ee5db3d79e0c096b527a84a3b25556f8e096
Techuang <chuangellow@gmail.com>
2023-12-31 15:15:59 +0800

    Solve 704. Binary Search in O(nlgn) time and O(1) space by binary search alogrithm

--- Array/704. Binary Search/main.cpp
+++ Array/704. Binary Search/main.cpp
@@ -6,7 +6,19 @@ using namespace std;
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        
+		// Time complexity: O(nlgn)
+		// Space complexity: O(1)
+		int n = nums.size();
+		if (n == 0 || n == 1 && nums.at(0) != target) return -1;
+		int left = 0, right = n - 1;
+		while (left <= right) {
+			// Why we use the left <= right instead of left < right?
+			int mid = (left + right) / 2;
+			if (nums.at(mid) == target) return mid;
+			else if (nums.at(mid) < target) left = mid + 1;
+			else right = mid - 1;
+		}
+		return -1;
     }
 };
 
