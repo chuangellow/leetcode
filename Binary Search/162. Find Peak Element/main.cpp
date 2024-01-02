ce694d4c8fa56842d56ee1d9e0cfe12e05433f6a
Techuang <chuangellow@gmail.com>
2024-01-02 19:23:22 +0800

    Solve 162. Find Peak Element in O(n) time and O(1) space by traversing

--- Binary Search/162. Find Peak Element/main.cpp
+++ Binary Search/162. Find Peak Element/main.cpp
@@ -6,7 +6,14 @@ using namespace std;
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        
+		int n = nums.size();
+		for (int i = 0; i < n; i++) {
+			bool flag = true;
+			if (i - 1 >= 0 && nums.at(i) <= nums.at(i-1)) flag = false;
+			if (i + 1 <= n - 1 && nums.at(i) <= nums.at(i+1)) flag = false;
+			if (flag) return i;
+		}
+		return -1;
     }
 };
 
