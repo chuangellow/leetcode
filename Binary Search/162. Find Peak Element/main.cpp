c2adc889cca61daa7af8366972455c744f5eba28
Techuang <chuangellow@gmail.com>
2024-01-04 00:47:38 +0800

    Solve 162. Find Peak Element in O(log(n)) time and O(1) space by binary search

--- Binary Search/162. Find Peak Element/main.cpp
+++ Binary Search/162. Find Peak Element/main.cpp
@@ -7,11 +7,15 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
 		int n = nums.size();
-		for (int i = 0; i < n; i++) {
-			bool flag = true;
-			if (i - 1 >= 0 && nums.at(i) <= nums.at(i-1)) flag = false;
-			if (i + 1 <= n - 1 && nums.at(i) <= nums.at(i+1)) flag = false;
-			if (flag) return i;
+		if (n == 1) return 0;
+		int left = 0, right = n - 1;
+		while (left <= right) {
+			int mid = (unsigned int) (left + right) >> 1;
+			if (mid - 1 < 0 && nums.at(mid) > nums.at(mid+1)) return mid;
+			if (mid + 1 >= n && nums.at(mid) > nums.at(mid-1)) return mid;
+			if (mid - 1 >= 0 && mid + 1 < n && nums.at(mid) > nums.at(mid-1) && nums.at(mid) > nums.at(mid+1)) return mid;
+			if (mid - 1 >= 0 && nums.at(mid-1) > nums.at(mid)) right = mid - 1;
+			else if (mid + 1 < n && nums.at(mid+1) > nums.at(mid)) left = mid + 1;
 		}
 		return -1;
     }
