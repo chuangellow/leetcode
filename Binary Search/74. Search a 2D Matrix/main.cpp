a36020220ce60049137ca3613be18033dd679438
Techuang <chuangellow@gmail.com>
2024-01-04 15:18:53 +0800

    Solve 74. Search a 2D Matrix in O(log(n*m)) time and O(1) space by binary search

--- Binary Search/74. Search a 2D Matrix/main.cpp
+++ Binary Search/74. Search a 2D Matrix/main.cpp
@@ -6,7 +6,38 @@ using namespace std;
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        
+        int n = matrix.size();
+        int m = matrix.at(0).size();
+        if (matrix.at(0).at(0) == target || matrix.at(n-1).at(m-1) == target)  return true;
+        if (target < matrix.at(0).at(0) && target > matrix.at(n-1).at(m-1)) return false;
+        int rowLeft = 0, rowRight = n - 1;
+        int colLeft = 0, colRight = m - 1;
+        while (colLeft <= colRight && rowLeft <= rowRight) {
+            int rowMid = (unsigned int) (rowLeft + rowRight) >> 1;
+            int colMid = (unsigned int) (colLeft + colRight) >> 1;
+            cout << "rowMid: " << rowMid << " " << "colMid: " << colMid << endl;
+            int midNum = matrix.at(rowMid).at(colMid);
+            if (midNum == target) return true;
+            if (midNum > target) {
+                if (target < matrix.at(rowMid).at(0)) {
+                    rowRight = rowMid - 1;
+                }
+                else {
+                    rowLeft = rowMid; rowRight = rowMid;
+                    colRight = colMid - 1;
+                }
+            }
+            else if (midNum < target) {
+                if (target > matrix.at(rowMid).at(m - 1)) {
+                    rowLeft = rowMid + 1;
+                }
+                else {
+                    rowLeft = rowMid; rowRight = rowMid;
+                    colLeft = colMid + 1;
+                }
+            }
+        }
+        return false;
     }
 };
 
