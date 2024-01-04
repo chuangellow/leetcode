54d5f1fe179fc6726271200e9baf21fbaffc0f9f
Techuang <chuangellow@gmail.com>
2024-01-04 17:11:45 +0800

    Solve 1351. Count Negative Numbers in a Sorted Matrix in O(n*m) time and O(1) space by linear traverse from the end

--- Binary Search/1351. Count Negative Numbers in a Sorted Matrix/main.cpp
+++ Binary Search/1351. Count Negative Numbers in a Sorted Matrix/main.cpp
@@ -5,22 +5,21 @@ using namespace std;
 
 class Solution {
 public:
-    int getRowNegatives(vector<vector<int>>& grid, int rowIdx, int cols) {
-        int left = 0, right = cols - 1;
-        while (left < right) {
-            int mid = (unsigned int) (left + right) >> 1;
-            int midNum = grid.at(rowIdx).at(mid);
-            if (midNum >= 0) left = mid + 1;
-            else right = mid;
-        }
-        return (grid.at(rowIdx).at(left) < 0)? cols - left: 0;
-    }
     int countNegatives(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            count += getRowNegatives(grid, i, m);
+        int rowIdx = n - 1, colIdx = m - 1;
+        while (rowIdx >= 0) {
+            int num = grid.at(rowIdx).at(colIdx);
+            if (num < 0) count++;
+            if (num > 0 || colIdx == 0) {
+                colIdx = m - 1;
+                rowIdx--;
+            }
+            else {
+                colIdx--;
+            }
         }
         return count;
     }
