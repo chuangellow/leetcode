a8bb83b6e0887a714e35faf428f80621d2dd6de6
Techuang <chuangellow@gmail.com>
2024-01-04 17:59:28 +0800

    Solve 70. Climbing Stairs in O(2^n) time and O(log(n)) space by recursive algorithm

--- Dynamic Programming/70. Climbing Stairs/main.cpp
+++ Dynamic Programming/70. Climbing Stairs/main.cpp
@@ -4,8 +4,19 @@ using namespace std;
 
 class Solution {
 public:
+    void countNumberWays(int current, int *ans, int n) {
+        if (current > n) return;
+        if (current == n) { 
+            *ans = *ans + 1;
+            return;
+        }
+        countNumberWays(current + 1, ans, n);
+        countNumberWays(current + 2, ans, n);
+    }
     int climbStairs(int n) {
-        
+        int ans = 0;
+        countNumberWays(0, &ans, n);
+        return ans;
     }
 };
 
