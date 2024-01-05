d5defbc08d55573e0773250840b7bb0c785f5f5d
Techuang <chuangellow@gmail.com>
2024-01-05 20:21:26 +0800

    Solve 740. Delete and Earn in O(n^2*2^n) time and O(log(n)) space by recursive algorithm

--- Dynamic Programming/740. Delete and Earn/main.cpp
+++ Dynamic Programming/740. Delete and Earn/main.cpp
@@ -5,8 +5,27 @@ using namespace std;
 
 class Solution {
 public:
+    void getMaxPoints(int currentPoint, int* maxPoint, vector<int> nums) {
+        int n = nums.size();
+        if (n == 0) {
+            *maxPoint = max(*maxPoint, currentPoint);
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            vector<int> temp;
+            for (int j = 0; j < n; j++) {
+                if (i != j && abs(nums.at(j) - nums.at(i)) != 1) {
+                    temp.push_back(nums.at(j));
+                }
+            }
+            getMaxPoints(currentPoint + nums.at(i), maxPoint, temp);
+        }
+        return;
+    }
     int deleteAndEarn(vector<int>& nums) {
-        
+        int maxPoint = -1;
+        getMaxPoints(0, &maxPoint, nums);
+        return maxPoint;
     }
 };
 
