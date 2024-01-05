8f1ec21d111e6c8a50aa761a1c2bed0271b96446
Techuang <chuangellow@gmail.com>
2024-01-05 16:30:14 +0800

    Solve 34. Find First and Last Position of Element in Sorted Array in O(log(n)) time and O(1) space by binary search twice

--- Binary Search/34. Find First and Last Position of Element in Sorted Array/main.cpp
+++ Binary Search/34. Find First and Last Position of Element in Sorted Array/main.cpp
@@ -5,8 +5,35 @@ using namespace std;
 
 class Solution {
 public:
+    int searchFirstIdx(vector<int> nums, int target, int size) {
+        int left = 0, right = size - 1;
+        while (left < right) {
+            int mid = (unsigned int) (left + right) >> 1;
+            if (nums.at(mid) < target) left = mid + 1;
+            else right = mid;
+        }
+        return (nums.at(right) == target)? right: -1;
+    }
+    int searchLastIdx(vector<int> nums, int target, int size) {
+        int left = 0, right = size - 1;
+        while (left < right) {
+            int mid = (unsigned int) (left + right + 1) >> 1;
+            if (nums.at(mid) > target) right = mid - 1;
+            else left = mid;
+        }
+        return (nums.at(right) == target)? right: -1;
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        
+        int n = nums.size(); 
+        vector<int> ret(2);
+        if (n < 1) {
+            ret = {-1, -1};
+            return ret;
+        }
+        int firstIdx = searchFirstIdx(nums, target, n);
+        int LastIdx = searchLastIdx(nums, target, n);
+        ret = {firstIdx, LastIdx};
+        return ret;
     }
 };
 
