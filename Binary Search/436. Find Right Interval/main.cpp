2a1828e89a8f59b84ce77b49e3a5d6690abb65c1
Techuang <chuangellow@gmail.com>
2024-01-05 17:10:31 +0800

    Solve 436. Find Right Interval in O(n^2) time and O(1) space by brute-force method

--- Binary Search/436. Find Right Interval/main.cpp
+++ Binary Search/436. Find Right Interval/main.cpp
@@ -6,16 +6,34 @@ using namespace std;
 class Solution {
 public:
     vector<int> findRightInterval(vector<vector<int>>& intervals) {
-        
+        int n = intervals.size();
+        vector<int> ret(n);
+        for (int i = 0; i < n; i++) {
+            int startNow = intervals.at(i).at(0);
+            int endNow = intervals.at(i).at(1);
+            int retIdx = -1;
+            int minStart = -1;
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                int startCompared = intervals.at(j).at(0);
+                int endCompared = intervals.at(j).at(1);
+                if (startCompared >= endNow && startCompared > minStart) {
+                    retIdx = j;
+                    minStart = startCompared;
+                }
+            }
+            ret.at(i) = retIdx;
+        }
+        return ret;
     }
 };
 
 int main(void) {
-    int n, m;
-    cin >> n >> m;
-    vector<vector<int>> intervals(n, vector<int>(m));
+    int n;
+    cin >> n;
+    vector<vector<int>> intervals(n, vector<int>(2));
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+        for (int j = 0; j < 2; j++) {
             cin >> intervals.at(i).at(j);
         }
     }
