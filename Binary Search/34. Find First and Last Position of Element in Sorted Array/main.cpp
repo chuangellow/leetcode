51e1f9fc5edd768aa9852bcaef0a40d056d4d578
Techuang <chuangellow@gmail.com>
2024-01-05 15:57:19 +0800

    Init 34. Find First and Last Position of Element in Sorted Array

--- Binary Search/34. Find First and Last Position of Element in Sorted Array/main.cpp
+++ Binary Search/34. Find First and Last Position of Element in Sorted Array/main.cpp
@@ -11,5 +11,14 @@ public:
 };
 
 int main(void) {
+    int n, target;
+    cin >> n >> target;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums.at(i);
+    }
+    Solution sol;
+    vector<int> ret = sol.searchRange(nums, target);
+    cout << "[" << ret.at(0) << "," << ret.at(1) << "]" << endl;
     return 0;
 }
