3331b924148ec13fbb9edd63841179b723555693
Techuang <chuangellow@gmail.com>
2024-01-05 19:46:53 +0800

    Solve 198. House Robber in O(2^n) time and O(log(n)) space by recursive algorithm

--- Dynamic Programming/198. House Robber/main.cpp
+++ Dynamic Programming/198. House Robber/main.cpp
@@ -5,8 +5,20 @@ using namespace std;
 
 class Solution {
 public:
+    void getMaxMoney(int pos, int currentMoney, int* maxMoney, vector<int>& nums, const int n) {
+        if (pos >= n) {
+            *maxMoney = (currentMoney > *maxMoney)? currentMoney: *maxMoney;
+            return;
+        }
+        getMaxMoney(pos+1, currentMoney, maxMoney, nums, n);
+        getMaxMoney(pos+2, currentMoney + nums.at(pos), maxMoney, nums, n);
+        return;
+    }
     int rob(vector<int>& nums) {
-        
+        int n = nums.size();
+        int maxMoney = -1;
+        getMaxMoney(0, 0, &maxMoney, nums, n);
+        return maxMoney;
     }
 };
 
