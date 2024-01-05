7b25e447bf1b57fec29de7c813f5a629f2ad5268
Techuang <chuangellow@gmail.com>
2024-01-05 18:18:27 +0800

    Solve 746. Min Cost Climbing Stairs in O(2^n) time and O(log(n)) space by resurive algorithm

--- Dynamic Programming/746. Min Cost Climbing Stairs/main.cpp
+++ Dynamic Programming/746. Min Cost Climbing Stairs/main.cpp
@@ -5,8 +5,21 @@ using namespace std;
 
 class Solution {
 public:
+    void findMinCost(int pos, int totalCost, int* minCost, vector<int>& cost, const int n) {
+        if (pos >= n) {
+            *minCost = (totalCost < *minCost)? totalCost: *minCost;
+            return;
+        }
+        findMinCost(pos + 1, totalCost + cost.at(pos), minCost, cost, n);
+        findMinCost(pos + 2, totalCost + cost.at(pos), minCost, cost, n);
+        return;
+    }
     int minCostClimbingStairs(vector<int>& cost) {
-        
+        int n = cost.size();
+        int minCost = INT32_MAX;
+        findMinCost(0, 0, &minCost, cost, n);
+        findMinCost(1, 0, &minCost, cost, n);
+        return minCost;
     }
 };
 
