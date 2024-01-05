43c892473087523d3532f7e2bfe3300c368a4099
Techuang <chuangellow@gmail.com>
2024-01-05 15:47:45 +0800

    Solve 1137. N-th Tribonacci Number in O(n) time and O(1) space by constant variables

--- Dynamic Programming/1137. N-th Tribonacci Number/main.cpp
+++ Dynamic Programming/1137. N-th Tribonacci Number/main.cpp
@@ -8,12 +8,20 @@ public:
     int tribonacci(int n) {
         if (n == 0) return 0;
         if (n == 1 || n == 2) return 1;
-        vector<int> dp(n+1, 0);
-        dp[0] = 0; dp[1] = 1; dp[2] = 1;
+        int number = 3;
+        vector<int> temp = {0, 1, 1};
+        int result = 0;
         for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+            result = 0;
+            for (int i = 0; i < number; i++) {
+                result += temp.at(i);
+            }
+            for (int i = 0; i < number-1; i++) {
+                temp[i] = temp[i+1];
+            }
+            temp[number-1] = result;
         }
-        return dp[n];
+        return result;
     }
 };
 
