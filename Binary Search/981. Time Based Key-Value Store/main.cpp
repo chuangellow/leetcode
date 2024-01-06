31534b2b5eda11e33aee1cd7481352c6aeaf3226
Techuang <chuangellow@gmail.com>
2024-01-06 15:08:05 +0800

    Solve 981. Time Based Key-Value Store in O(nlog(n)) time and O(n^2) space by hashing and binary search

--- Binary Search/981. Time Based Key-Value Store/main.cpp
+++ Binary Search/981. Time Based Key-Value Store/main.cpp
@@ -1,21 +1,46 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
 class TimeMap {
 public:
+    unordered_map<string, vector<pair<string, int>>*> table;
+
     TimeMap() {
-        
     }
-    
+
+    int search(vector<pair<string, int>>& secondLevelTable, int target) {
+        int n = secondLevelTable.size();
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int mid = (unsigned int) (left + right + 1) >> 1;
+            if (secondLevelTable.at(mid).second <= target) left = mid;
+            else right = mid - 1;
+        }
+        return (secondLevelTable.at(left).second <= target)? left: -1;
+    }
     void set(string key, string value, int timestamp) {
-        
+        if (table.find(key) == table.end()) {
+            vector<pair<string, int>>* secondLevelTable = new vector<pair<string, int>>();
+            secondLevelTable->push_back(pair(value, timestamp));
+            table[key] = secondLevelTable;
+            return;
+        }
+        vector<pair<string, int>>* secondLevelTable = table[key];
+        int insertIdx = search(*secondLevelTable, timestamp);
+        secondLevelTable->insert(secondLevelTable->begin() + insertIdx + 1, pair(value, timestamp));
+        return;
     }
     
     string get(string key, int timestamp) {
-        
+        if (table.find(key) == table.end()) return "";
+        vector<pair<string, int>>* secondLevelTable = table[key];
+        int idx = search(*secondLevelTable, timestamp);
+        if (idx == -1) return "";
+        return secondLevelTable->at(idx).first;
     }
 };
 
