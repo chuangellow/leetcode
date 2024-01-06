575824916a74bb9762e66460908cedeace6eec9c
Techuang <chuangellow@gmail.com>
2024-01-06 15:36:49 +0800

    Init 62. Unique Paths

---
+++ Dynamic Programming/62. Unique Paths/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        
+    }
+};
+
+int main(void) {
+    int m, n;
+    cin >> m >> n;
+    Solution sol;
+    cout << sol.uniquePaths(m, n) << endl;
+    return 0;
+}
