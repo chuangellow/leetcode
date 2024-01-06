61dbec72cb8f4ba5c358a4c1f5d9b700ee5cd86a
Techuang <chuangellow@gmail.com>
2024-01-06 16:44:26 +0800

    Refine the dp solution on 64. Minimum Path Sum to reduce the memory usage by using 1D table

--- Dynamic Programming/64. Minimum Path Sum/main.cpp
+++ Dynamic Programming/64. Minimum Path Sum/main.cpp
@@ -8,23 +8,25 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n));
+        vector<int> dp(n, 0);
         int sum = 0;
         for (int i = 0; i < n; i++) {
-            sum += grid.at(0).at(i); 
-            dp.at(0).at(i) = sum;
+            sum += grid.at(0).at(i);
+            dp.at(i) = sum;
+            cout << dp.at(i) << " ";
         }
-        sum = 0;
-        for (int i = 0; i < m; i++) {
-            sum += grid.at(i).at(0);
-            dp.at(i).at(0) = sum;
-        }
+        sum = grid.at(0).at(0);
         for (int i = 1; i < m; i++) {
+            sum += grid.at(i).at(0);
+            int temp = sum;
             for (int j = 1; j < n; j++) {
-                dp.at(i).at(j) = min(dp.at(i-1).at(j), dp.at(i).at(j-1)) + grid.at(i).at(j);
+                temp = min(dp.at(j), temp) + grid.at(i).at(j);
+                dp.at(j) = temp;
+                cout << temp << " ";
             }
+            cout << endl;
         }
-        return dp.at(m-1).at(n-1);
+        return dp.at(n-1);
     }
 };
 
