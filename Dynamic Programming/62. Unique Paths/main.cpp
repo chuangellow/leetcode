9c97bcae1e928503254a25f0c8e6c52a672d86f4
Techuang <chuangellow@gmail.com>
2024-01-06 16:07:16 +0800

    Refine the solution on 62. Unique Paths to reduce the memory usage by 1D table

--- Dynamic Programming/62. Unique Paths/main.cpp
+++ Dynamic Programming/62. Unique Paths/main.cpp
@@ -6,19 +6,15 @@ using namespace std;
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        for (int i = 0; i < n; i++) {
-            dp.at(0).at(i) = 1;
-        }
-        for (int i = 0; i < m; i++) {
-            dp.at(i).at(0) = 1;
-        }
+        vector<int> prevRow(n, 1);
         for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp.at(i).at(j) = dp.at(i-1).at(j) + dp.at(i).at(j-1);
+            int temp = 1;
+            for (int i = 1; i < n; i++) {
+                temp = prevRow.at(i) + temp;
+                prevRow.at(i) = temp;
             }
         }
-        return dp.at(m-1).at(n-1);
+        return prevRow.at(n-1);
     }
 };
 
