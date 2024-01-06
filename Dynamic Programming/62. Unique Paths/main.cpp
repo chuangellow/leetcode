7943bc194a78911ea40402eb8377949cd1a7f489
Techuang <chuangellow@gmail.com>
2024-01-06 15:42:59 +0800

    Solve 62. Unique Paths in O(2^(m+n)) time and O(log(m+n)) space by recursive alogrithm

--- Dynamic Programming/62. Unique Paths/main.cpp
+++ Dynamic Programming/62. Unique Paths/main.cpp
@@ -5,8 +5,23 @@ using namespace std;
 
 class Solution {
 public:
+    void getNumPaths(int currentM, int currentN, int* numPaths, const int m, const int n) {
+        if (currentM == m - 1 && currentN == n - 1) {
+            *numPaths = *numPaths + 1;
+            return;
+        }
+        if (currentM != m - 1) {
+            getNumPaths(currentM+1, currentN, numPaths, m, n);
+        }
+        if (currentN != n - 1) {
+            getNumPaths(currentM, currentN+1, numPaths, m, n);
+        }
+        return;
+    }
     int uniquePaths(int m, int n) {
-        
+        int numPaths = 0;
+        getNumPaths(0, 0, &numPaths, m, n);
+        return numPaths;
     }
 };
 
