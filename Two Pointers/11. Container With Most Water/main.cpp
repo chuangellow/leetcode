6790696be18f6be361f31c9a2af30038f0462f9a
Techuang <chuangellow@gmail.com>
2024-01-07 15:10:29 +0800

    Solve 11. Container With Most Water in O(n^2) time and O(1) time by brute-force method

--- Two Pointers/11. Container With Most Water/main.cpp
+++ Two Pointers/11. Container With Most Water/main.cpp
@@ -6,14 +6,21 @@ using namespace std;
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        
+        int n = height.size();
+        int maxAmount = -1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                maxAmount = max(maxAmount, (j - i) * min(height.at(i), height.at(j)));
+            }
+        }
+        return maxAmount;
     }
 };
 
 int main(void) {
     int n;
     cin >> n;
-    vector<int> height;
+    vector<int> height(n);
     for (int i = 0; i < n; i++) {
         cin >> height.at(i);
     }
