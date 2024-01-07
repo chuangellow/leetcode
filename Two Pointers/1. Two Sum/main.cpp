60439b6bb4107dcf462f7045f198eede3424b629
Techuang <chuangellow@gmail.com>
2024-01-07 13:56:06 +0800

    Solve 1. Two Sum in O(nlogn) time and O(n) space by sorting and binary search

--- Two Pointers/1. Two Sum/main.cpp
+++ Two Pointers/1. Two Sum/main.cpp
@@ -5,13 +5,27 @@ using namespace std;
 
 class Solution {
 public:
+    int binarySearch(int start, vector<pair<int, int>>& nums, int target, int n) {
+        int left = start, right = n-1;
+        while (left <= right) {
+            int mid = (unsigned int) (left + right) >> 1;
+            if (nums.at(mid).first == target) return mid;
+            else if (nums.at(mid).first > target) right = mid - 1;
+            else left = mid + 1;
+        }
+        return -1;
+    }
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
         vector<int> ans;
+        vector<pair<int, int>> temp(n);
         for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
-                if (nums.at(i) + nums.at(j) == target) ans = {i, j};
-            }
+            temp.at(i) = pair(nums.at(i), i);
+        }
+        sort(temp.begin(), temp.end());
+        for (int i = 0; i < n; i++) {
+            int idx = binarySearch(i+1, temp, target-temp.at(i).first, n);
+            if (idx != -1) ans = {temp.at(i).second, temp.at(idx).second};
         }
         return ans;
     }
