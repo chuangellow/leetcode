b2d061da7e43224186b4f1c935f9b95d35cd528f
Techuang <chuangellow@gmail.com>
2024-01-07 15:32:40 +0800

    Solve 11. Container With Most Water in O(n) time and O(1) space by two pointers method

--- Two Pointers/11. Container With Most Water/main.cpp
+++ Two Pointers/11. Container With Most Water/main.cpp
@@ -8,10 +8,12 @@ public:
     int maxArea(vector<int>& height) {
         int n = height.size();
         int maxAmount = -1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                maxAmount = max(maxAmount, (j - i) * min(height.at(i), height.at(j)));
-            }
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int amount = (right - left) * (min(height.at(left), height.at(right)));
+            maxAmount = max(amount, maxAmount);
+            if (height.at(left) < height.at(right)) left++;
+            else right--;
         }
         return maxAmount;
     }
