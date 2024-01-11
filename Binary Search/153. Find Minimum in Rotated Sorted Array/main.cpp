e27bdd6d98b1fe80e17a9ea7b92062b68db8b689
Techuang <chuangellow@gmail.com>
2024-01-11 15:42:18 +0800

    Solve 153. Find Minimum in Rotated Sorted Array in O(log(n)) time and O(1) space by binary search of the rotated pivot

--- Binary Search/153. Find Minimum in Rotated Sorted Array/main.cpp
+++ Binary Search/153. Find Minimum in Rotated Sorted Array/main.cpp
@@ -5,8 +5,21 @@ using namespace std;
 
 class Solution {
 public:
+    int findPivot(vector<int>& nums, const int n) {
+        if (nums.at(0) < nums.at(n-1)) return n-1;
+        int left = 0, right = n-1;
+        int rightMostNum = nums.at(n-1);
+        while (left < right) {
+            int mid = (unsigned int) (left + right + 1) >> 1;
+            if (nums.at(mid) > rightMostNum) left = mid;
+            else right = mid - 1;
+        }
+        return left;
+    }
     int findMin(vector<int>& nums) {
-        
+        int n = nums.size();
+        int pivot = findPivot(nums, n);
+        return nums.at((pivot+1)%n);
     }
 };
 
