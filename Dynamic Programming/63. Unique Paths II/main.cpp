f2481e21b07a64ade0fb8928918f242fa4004a7a
Techuang <chuangellow@gmail.com>
2024-01-11 17:16:28 +0800

    Refine the solution on 63. Unique Paths II by 1D dp table to reduce the memory usage

--- Dynamic Programming/63. Unique Paths II/main.cpp
+++ Dynamic Programming/63. Unique Paths II/main.cpp
@@ -8,20 +8,18 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        dp[0][0] = (!obstacleGrid[0][0]);
+        vector<int> dp(n);
+        dp[0] = (!obstacleGrid[0][0]);
+        for (int i = 1; i < n; i++) dp[i] = dp[i-1] && (!obstacleGrid[0][i]);
         for (int i = 1; i < m; i++) {
-            dp[i][0] = dp[i-1][0] && (!obstacleGrid[i][0]);
-        }
-        for (int j = 1; j < n; j++) {
-            dp[0][j] = dp[0][j-1] && (!obstacleGrid[0][j]);
-        }
-        for (int i = 1; i < m; i++) {
+            int temp = dp[0] && (!obstacleGrid[i][0]);
+            dp[0] = temp;
             for (int j = 1; j < n; j++) {
-                dp[i][j] = (obstacleGrid[i][j])? 0: (dp[i-1][j] + dp[i][j-1]);
+                temp = (obstacleGrid[i][j])? 0: temp + dp[j];
+                dp[j] = temp;
             }
         }
-        return (obstacleGrid[m-1][n-1])? 0: dp[m-1][n-1];
+        return (obstacleGrid[m-1][n-1])? 0: dp[n-1];
     }
 };
 
