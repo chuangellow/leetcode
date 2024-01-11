5ef16ee4c4e7a8194e1ddd2d83a08207b7ec4ec2
Techuang <chuangellow@gmail.com>
2024-01-11 17:04:23 +0800

    Solve 63. Unique Paths II in O(m*n) time and O(m*n) space by dynamic programming

--- Dynamic Programming/63. Unique Paths II/main.cpp
+++ Dynamic Programming/63. Unique Paths II/main.cpp
@@ -5,25 +5,23 @@ using namespace std;
 
 class Solution {
 public:
-    void getNumPaths(int x, int y, int* numPaths, vector<vector<int>>& obstacleGrid, const int m, const int n) {
-        if (x == m - 1 && y == n - 1) {
-            *numPaths = *numPaths + 1;
-            return;
-        }
-        if (x < m - 1 && !obstacleGrid[x+1][y]) {
-            getNumPaths(x+1, y, numPaths, obstacleGrid, m, n);
-        }
-        if (y < n - 1 && !obstacleGrid[x][y+1]) {
-            getNumPaths(x, y+1, numPaths, obstacleGrid, m, n);
-        }
-        return;
-    }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int numPaths = 0;
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        getNumPaths(0, 0, &numPaths, obstacleGrid, m, n);
-        return numPaths;
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        dp[0][0] = (!obstacleGrid[0][0]);
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i-1][0] && (!obstacleGrid[i][0]);
+        }
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = dp[0][j-1] && (!obstacleGrid[0][j]);
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = (obstacleGrid[i][j])? 0: (dp[i-1][j] + dp[i][j-1]);
+            }
+        }
+        return (obstacleGrid[m-1][n-1])? 0: dp[m-1][n-1];
     }
 };
 
