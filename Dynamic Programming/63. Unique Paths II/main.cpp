576f52b1f28611c7f6c0c3d1890250bf7f7da3ea
Techuang <chuangellow@gmail.com>
2024-01-11 16:54:03 +0800

    Solve 63. Unique Paths II in O(2^(m+n)) time and O(log(m+n)) space by recursive method

--- Dynamic Programming/63. Unique Paths II/main.cpp
+++ Dynamic Programming/63. Unique Paths II/main.cpp
@@ -5,8 +5,25 @@ using namespace std;
 
 class Solution {
 public:
+    void getNumPaths(int x, int y, int* numPaths, vector<vector<int>>& obstacleGrid, const int m, const int n) {
+        if (x == m - 1 && y == n - 1) {
+            *numPaths = *numPaths + 1;
+            return;
+        }
+        if (x < m - 1 && !obstacleGrid[x+1][y]) {
+            getNumPaths(x+1, y, numPaths, obstacleGrid, m, n);
+        }
+        if (y < n - 1 && !obstacleGrid[x][y+1]) {
+            getNumPaths(x, y+1, numPaths, obstacleGrid, m, n);
+        }
+        return;
+    }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        
+        int numPaths = 0;
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        getNumPaths(0, 0, &numPaths, obstacleGrid, m, n);
+        return numPaths;
     }
 };
 
