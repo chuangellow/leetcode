48c7d31eda0c5ebbbcd6a7c7691d777ea8e284ee
Techuang <chuangellow@gmail.com>
2024-01-13 09:51:42 +0800

    Solve 120. Triangle in O(2^L) time and O(L) space by recursive method

--- Dynamic Programming/120. Triangle/main.cpp
+++ Dynamic Programming/120. Triangle/main.cpp
@@ -5,8 +5,28 @@ using namespace std;
 
 class Solution {
 public:
+    void getMinTotal(int previous, int layer, int value, int* minTotal, vector<vector<int>>& triangle, int n) {
+        if (layer == n) {
+            *minTotal = min(*minTotal, value);
+            return;
+        }
+        int size = triangle.at(layer).size();
+        if (previous == -1) {
+            for (int i = 0; i < size; i++) {
+                getMinTotal(i, layer+1, value + triangle.at(layer).at(i), minTotal, triangle, n);
+            }
+        }
+        else {
+            getMinTotal(previous, layer+1, value + triangle.at(layer).at(previous), minTotal, triangle, n);
+            getMinTotal(previous+1, layer+1, value + triangle.at(layer).at(previous+1), minTotal, triangle, n);
+        }
+        return;
+    }
     int minimumTotal(vector<vector<int>>& triangle) {
-        return 0;
+        int minTotal = INT32_MAX;
+        int n = triangle.size();
+        getMinTotal(-1, 0, 0, &minTotal, triangle, n);
+        return minTotal;
     }
 };
 
