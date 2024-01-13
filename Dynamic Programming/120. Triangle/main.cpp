a5c082067642a7efe456bf98e34391a8857d45ea
Techuang <chuangellow@gmail.com>
2024-01-13 10:20:28 +0800

    Refine the solution on 120. Triangle by 1D table to reduce the memory usage

--- Dynamic Programming/120. Triangle/main.cpp
+++ Dynamic Programming/120. Triangle/main.cpp
@@ -7,23 +7,25 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        int m = triangle.at(n-1).size();
-        vector<vector<int>> dp;
-        dp.push_back(vector<int> {triangle.at(0).at(0)});
-        if (n == 1) return dp.at(0).at(0);
+        vector<int> dp(n, 0);
+        dp.at(0) = triangle.at(0).at(0);
+        if (n == 0) return dp.at(0);
         for (int i = 1; i < n; i++) {
-            int size = triangle.at(i).size();
-            vector<int> dpNums(size);
-            for (int j = 0; j < size; j++) {
-                if (j == 0) dpNums.at(j) = dp.at(i-1).at(j) + triangle.at(i).at(j);
-                else if (j == size-1) dpNums.at(j) = dp.at(i-1).at(j-1) + triangle.at(i).at(j);
-                else dpNums.at(j) = min(dp.at(i-1).at(j), dp.at(i-1).at(j-1)) + triangle.at(i).at(j);
+            vector<int> tempNums(n, 0);
+            for (int j = 0; j < i+1; j++) {
+                int temp = triangle.at(i).at(j);
+                if (j == 0) temp += dp.at(j);
+                else if (j == i) temp += dp.at(j-1);
+                else temp += min(dp.at(j), dp.at(j-1));
+                tempNums.at(j) = temp;
             }
-            dp.push_back(dpNums);
+            for (int j = 0; j < i+1; j++) {
+                dp.at(j) = tempNums.at(j);
+            }
         }
         int minValue = INT32_MAX;
-        for (int i = 0; i < m; i++) {
-            minValue = min(dp.at(n-1).at(i), minValue);
+        for (int i = 0; i < n; i++) {
+            minValue = min(dp.at(i), minValue);
         }
         return minValue;
     }
