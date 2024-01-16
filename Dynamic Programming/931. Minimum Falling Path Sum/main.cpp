0958f6c3c6082ff4dd1f4a11b1e40144c96fb3ac
Techuang <chuangellow@gmail.com>
2024-01-16 23:00:26 +0800

    Solve 931. Minimum Falling Path Sum in O(2^n) time and O(n) space by recursive method

--- Dynamic Programming/931. Minimum Falling Path Sum/main.cpp
+++ Dynamic Programming/931. Minimum Falling Path Sum/main.cpp
@@ -5,8 +5,23 @@ using namespace std;
 
 class Solution {
 public:
+    void findMinPathSum(int x, int y, int currentSum, int* minPathSum, vector<vector<int>>& matrix, const int n) {
+        if (x >= n) {
+            *minPathSum = min(currentSum, *minPathSum);
+            return;
+        }
+        if (y - 1 >= 0) findMinPathSum(x+1, y-1, currentSum + matrix.at(x).at(y), minPathSum, matrix, n);
+        if (y + 1 < n) findMinPathSum(x+1, y+1, currentSum + matrix.at(x).at(y), minPathSum, matrix, n);
+        findMinPathSum(x+1, y, currentSum + matrix.at(x).at(y), minPathSum, matrix, n);
+        return;
+    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        
+        int n = matrix.size();
+        int minPathSum = INT32_MAX;
+        for (int i = 0; i < n; i++) {
+            findMinPathSum(0, i, 0, &minPathSum, matrix, n);
+        }
+        return minPathSum;
     }
 };
 
