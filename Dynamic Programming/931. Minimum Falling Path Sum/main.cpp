cddbfd5bbae6373e5ebf9bc6a50f4e6d28008491
Techuang <chuangellow@gmail.com>
2024-01-16 23:23:00 +0800

    Refine 931. Minimum Falling Path Sum by 1D table to reduce the memory usage

--- Dynamic Programming/931. Minimum Falling Path Sum/main.cpp
+++ Dynamic Programming/931. Minimum Falling Path Sum/main.cpp
@@ -7,20 +7,24 @@ class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-        for (int j = 0; j < n; j++) dp[0][j] = matrix[0][j];
+        vector<int> dp(n, 0);
+        for (int i = 0; i < n; i++) dp[i] = matrix[0][i];
         for (int i = 1; i < n; i++) {
+            vector<int> temp(n, 0);
             for (int j = 0; j < n; j++) {
                 int minValue = INT32_MAX;
-                if (j-1 >= 0) minValue = min(dp[i-1][j-1], minValue);
-                if (j+1 < n) minValue = min(dp[i-1][j+1], minValue);
-                minValue = min(dp[i-1][j], minValue);
-                dp[i][j] = minValue + matrix[i][j];
+                if (j - 1 >= 0) minValue = min(minValue, dp[j-1]);
+                if (j + 1 < n) minValue = min(minValue, dp[j+1]);
+                minValue = min(minValue, dp[j]);
+                temp[j] = minValue + matrix[i][j];
+            }
+            for (int j = 0; j < n; j++) {
+                dp[j] = temp[j];
             }
         }
         int minValue = INT32_MAX;
-        for (int j = 0; j < n; j++) {
-            minValue = min(minValue, dp[n-1][j]);
+        for (int i = 0; i < n; i++) {
+            minValue = min(minValue, dp[i]);
         }
         return minValue;
     }
