285af8a3747175b16bce71e13bb22e7cf4f8a486
Techuang <chuangellow@gmail.com>
2024-01-17 17:46:23 +0800

    Solve 5. Longest Palindromic Substring in O(n^2) time and O(n) space by expanding from center

--- Dynamic Programming/5. Longest Palindromic Substring/main.cpp
+++ Dynamic Programming/5. Longest Palindromic Substring/main.cpp
@@ -1,62 +1,52 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
+    string remove(string& s, char target) {
+        string output;
+        int start = 0, end = s.length();
+        int insertIdx = 0;
+        for (int i = 0; i < end; i++) {
+            if (s.at(i) != target) {
+                output.push_back(s.at(i));
+                insertIdx++;
+            }
+        }
+        return output;
+    }
     string longestPalindrome(string s) {
         int len = s.length();
         if (len == 1) return s.substr(0, 1);
         if (len == 2) return (s[0] == s[1])? s.substr(0, 2): s.substr(0, 1);
-        int tableLen = (len - 2) * 2 - 1;
-        vector<int> dp(tableLen, 0);
+        string strWithDelim;
+        for (int i = 0; i < 2 * len + 1; i++) {
+            if (i % 2 == 0) strWithDelim.push_back('*');
+            else strWithDelim.push_back(s.at(i/2));
+        }
         int maxLen = -1;
         int maxLeft = -1, maxRight = -1;
-        if (s[0] == s[1]) {
-            maxLen = 2;
-            maxLeft = 0, maxRight = 1;
-        }
-        if (s[len-2] == s[len-1]) {
-            maxLen = 2;
-            maxLeft = len-2, maxRight = len-1;
-        }
-        for (int i = 0; i < tableLen; i++) {
-            int index = (i / 2) + 1;
-            if (i % 2 == 0) {
-                dp[i] = 1;
-                if (dp[i] > maxLen) {
-                    maxLen = dp[i]; 
-                    maxLeft = index; 
-                    maxRight = index;
+        for (int i = 0; i < 2 * len + 1; i++) {
+            int currentLen = (strWithDelim[i] == '*')? 1 : 2;
+            while (i - currentLen >= 0 && i + currentLen < 2 * len + 1) {
+                if (strWithDelim[i-currentLen] == strWithDelim[i+currentLen]) {
+                    if (2 * currentLen + 1 > maxLen) {
+                        maxLen = 2 * currentLen + 1;
+                        maxLeft = i - currentLen;
+                        maxRight = i + currentLen;
+                    }
+                    currentLen += 2;
                 }
-            }
-            if (i % 2 == 1 && s[index] == s[index + 1]) {
-                dp[i] = 2;
-                if (dp[i] > maxLen) {
-                    maxLen = dp[i]; 
-                    maxLeft = index; 
-                    maxRight = index+1;
-                }
+                else break;
             }
         }
-        int width = 2;
-        int left = 0, right = left + width;
-        while (right < len) {
-            while (right < len) {
-                int index = left + right - 2; 
-                dp[index] = (dp[index] != 0 && s[left] == s[right])? dp[index] + 2: 0;
-                if (dp[index] > maxLen) {
-                    maxLen = dp[index];
-                    maxLeft = left, maxRight = right;
-                }
-                left++;
-                right++;
-            }
-            width++;
-            left = 0, right = left + width;
-        }
-        return s.substr(maxLeft, maxRight-maxLeft+1);
+        if (maxLeft == -1 && maxRight == -1) return s.substr(0, 1);
+        strWithDelim = strWithDelim.substr(maxLeft, maxRight - maxLeft + 1);
+        string out = remove(strWithDelim, '*');
+        return out;
     }
 };
 
