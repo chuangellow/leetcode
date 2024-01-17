d8dcb44c53b3a420994268bb1937073f822e2a5a
Techuang <chuangellow@gmail.com>
2024-01-17 14:31:05 +0800

    Solve 5. Longest Palindromic Substring in O(n^3) time and O(1) space by brute-force method

--- Dynamic Programming/5. Longest Palindromic Substring/main.cpp
+++ Dynamic Programming/5. Longest Palindromic Substring/main.cpp
@@ -5,7 +5,34 @@ using namespace std;
 
 class Solution {
 public:
+    bool checkPalindrome(int left, int right, string s) {
+        while (left <= right) {
+            if (s.at(left) == s.at(right)) {
+                left++;
+                right--;
+            }
+            else return false;
+        }
+        return true;
+    }
     string longestPalindrome(string s) {
+        int len = s.length();
+        int maxLen = -1;
+        string outStr;
+        int start = -1, end = -1;
+        for (int i = 0; i < len; i++) {
+            for (int j = i+1; j < len; j++) {
+                if (checkPalindrome(i, j, s)) {
+                    if (j - i + 1 > maxLen) {
+                        maxLen = j - i + 1;
+                        start = i, end = j;
+                        cout << i << ' ' << j << endl;
+                    }
+                }
+            }
+        }
+        if (start == -1 && end == -1) return s.substr(0, 1);
+        return s.substr(start, end-start+1);
     }
 };
 
