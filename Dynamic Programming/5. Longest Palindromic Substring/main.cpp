ff9f13844af4e8d47bd90aaf6cf72c6b408517b0
Techuang <chuangellow@gmail.com>
2024-01-17 16:13:02 +0800

    Refine /5. Longest Palindromic Substring by 1D table to reduce the memory usage

--- Dynamic Programming/5. Longest Palindromic Substring/main.cpp
+++ Dynamic Programming/5. Longest Palindromic Substring/main.cpp
@@ -7,31 +7,54 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int len = s.length();
-        vector<vector<int>> dp(len, vector<int>(len, 0));
-        for (int i = 0; i < len; i++) {
-            dp[i][i] = 1;
-            if (i+1 < len && s[i] == s[i+1]) dp[i][i+1] = 2;
+        if (len == 1) return s.substr(0, 1);
+        if (len == 2) return (s[0] == s[1])? s.substr(0, 2): s.substr(0, 1);
+        int tableLen = (len - 2) * 2 - 1;
+        vector<int> dp(tableLen, 0);
+        int maxLen = -1;
+        int maxLeft = -1, maxRight = -1;
+        if (s[0] == s[1]) {
+            maxLen = 2;
+            maxLeft = 0, maxRight = 1;
+        }
+        if (s[len-2] == s[len-1]) {
+            maxLen = 2;
+            maxLeft = len-2, maxRight = len-1;
+        }
+        for (int i = 0; i < tableLen; i++) {
+            int index = (i / 2) + 1;
+            if (i % 2 == 0) {
+                dp[i] = 1;
+                if (dp[i] > maxLen) {
+                    maxLen = dp[i]; 
+                    maxLeft = index; 
+                    maxRight = index;
+                }
+            }
+            if (i % 2 == 1 && s[index] == s[index + 1]) {
+                dp[i] = 2;
+                if (dp[i] > maxLen) {
+                    maxLen = dp[i]; 
+                    maxLeft = index; 
+                    maxRight = index+1;
+                }
+            }
         }
         int width = 2;
         int left = 0, right = left + width;
         while (right < len) {
             while (right < len) {
-                dp[left][right] = (dp[left+1][right-1] != 0 && s[left] == s[right])? dp[left+1][right-1] + 2: 0;
+                int index = left + right - 2; 
+                dp[index] = (dp[index] != 0 && s[left] == s[right])? dp[index] + 2: 0;
+                if (dp[index] > maxLen) {
+                    maxLen = dp[index];
+                    maxLeft = left, maxRight = right;
+                }
                 left++;
                 right++;
             }
             width++;
             left = 0, right = left + width;
-        }
-        int maxLen = -1;
-        int maxLeft = -1, maxRight = -1;
-        for (int i = 0; i < len; i++) {
-            for (int j = i; j < len; j++) {
-                if (dp[i][j] > maxLen) {
-                    maxLen = dp[i][j];
-                    maxLeft = i, maxRight = j;
-                }
-            }
         }
         return s.substr(maxLeft, maxRight-maxLeft+1);
     }
--- README.md
+++ README.md
@@ -157,7 +157,7 @@ The problems are organized into directories based on the main technique or data 
 | 120 | [Triangle](https://leetcode.com/problems/triangle/)| [C++](./Dynamic%20Programming/120.%20Triangle/main.cpp) | | Medium | 2024/01/13 | [x] | [ ] |
 | 931 | [Minimum Falling Path Sum](https://leetcode.com/problems/minimum-falling-path-sum/)| [C++](./Dynamic%20Programming/931.%20Minimum%20Falling%20Path%20Sum/main.cpp) | | Medium | 2024/01/16 | [x] | [ ] |
 | 221 | [Maximal Square](https://leetcode.com/problems/maximal-square/)| [C++](./Dynamic%20Programming/221.%20Maximal%20Square/main.cpp) | | Medium | 2024/01/17 | [x] | [ ] |
-| 5 | [Longest Palindromic Substring](https://leetcode.com/problems/longest-palindromic-substring/)| [C++](./Dynamic%20Programming/221.%20Maximal%20Square/main.cpp) | | Medium | 2024/01/17 | [ ] | [ ] |
+| 5 | [Longest Palindromic Substring](https://leetcode.com/problems/longest-palindromic-substring/)| [C++](./Dynamic%20Programming/221.%20Maximal%20Square/main.cpp) | | Medium | 2024/01/17 | [x] | [ ] |
 
 # Reference：
 
