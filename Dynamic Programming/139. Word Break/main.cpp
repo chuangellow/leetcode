31830b5b767ce72a40ab2ccec0c9f405980cee58
Techuang <chuangellow@gmail.com>
2024-01-18 13:13:57 +0800

    Solve 139. Word Break in O(n^2*m*k) time and O(n) space by recursive method

--- Dynamic Programming/139. Word Break/main.cpp
+++ Dynamic Programming/139. Word Break/main.cpp
@@ -6,8 +6,25 @@ using namespace std;
 
 class Solution {
 public:
+    bool findInDict(int left, int right, string s, vector<string>& wordDict, int dictSize) {
+        for (int i = 0; i < dictSize; i++) {
+            if (s.substr(left, right-left+1).compare(wordDict[i]) == 0) return true;
+        }
+        return false;
+    }
+    bool solve(int start, string s, vector<string>& wordDict, int strLen, int dictSize) {
+        if (start == strLen) return true;
+        for (int end = start; end < strLen; end++) {
+            if (findInDict(start, end, s, wordDict, dictSize) && solve(end + 1, s, wordDict, strLen, dictSize)) {
+                return true;
+            }
+        }
+        return false;
+    }
     bool wordBreak(string s, vector<string>& wordDict) {
-        
+        int strLen = s.length();
+        int dictSize = wordDict.size();
+        return solve(0, s, wordDict, strLen, dictSize);
     }
 };
 
