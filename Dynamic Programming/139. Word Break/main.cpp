c616d575de87ee4cb61e05f41ac081fa4c3bdaae
Techuang <chuangellow@gmail.com>
2024-01-18 14:24:45 +0800

    Solve 139. Word Break in O(n^3) time and O(n^2) space by dynamic programming

--- Dynamic Programming/139. Word Break/main.cpp
+++ Dynamic Programming/139. Word Break/main.cpp
@@ -1,30 +1,37 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
 class Solution {
 public:
-    bool findInDict(int left, int right, string s, vector<string>& wordDict, int dictSize) {
-        for (int i = 0; i < dictSize; i++) {
-            if (s.substr(left, right-left+1).compare(wordDict[i]) == 0) return true;
-        }
-        return false;
-    }
-    bool solve(int start, string s, vector<string>& wordDict, int strLen, int dictSize) {
-        if (start == strLen) return true;
-        for (int end = start; end < strLen; end++) {
-            if (findInDict(start, end, s, wordDict, dictSize) && solve(end + 1, s, wordDict, strLen, dictSize)) {
-                return true;
-            }
-        }
-        return false;
-    }
     bool wordBreak(string s, vector<string>& wordDict) {
         int strLen = s.length();
-        int dictSize = wordDict.size();
-        return solve(0, s, wordDict, strLen, dictSize);
+        vector<vector<bool>> dp(strLen, vector<bool>(strLen, false));
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        for (int i = 0; i < strLen; i++) {
+            if (dict.find(s.substr(i, 1)) != dict.end()) {
+                dp[i][i] = true;
+            }
+        }
+        for (int len = 2; len <= strLen; len++) {
+            for (int i = 0; i <= strLen - len; i++) {
+                int j = i + len - 1;
+                if (dict.find(s.substr(i, len)) != dict.end()) {
+                    dp[i][j] = true;
+                } else {
+                    for (int k = i; k < j; k++) {
+                        if (dp[i][k] && dp[k+1][j]) {
+                            dp[i][j] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return dp[0][strLen-1];
     }
 };
 
