f98da29466646a46e937dd349afccbc8a64b770a
Techuang <chuangellow@gmail.com>
2024-01-18 16:43:46 +0800

    Solve 208. Implement Trie (Prefix Tree)

--- Tree/208. Implement Trie (Prefix Tree)/main.cpp
+++ Tree/208. Implement Trie (Prefix Tree)/main.cpp
@@ -4,15 +4,54 @@
 
 using namespace std;
 
+struct TrieNode {
+    struct TrieNode* children[26] = {nullptr};
+    bool flag = false;
+};
+
 class Trie {
 public:
+    TrieNode* root;
     Trie() {
+        root = new TrieNode();
+        for (int i = 0; i < 26; i++) {
+            root->children[i] = nullptr;
+        }
     }
     void insert(string word) {
+        TrieNode* currentNode = root;
+        for (char c: word) {
+            if (currentNode->children[c-'a'] == nullptr) {
+                TrieNode* newNode = new TrieNode();
+                currentNode->children[c-'a'] = newNode;
+            }
+            currentNode = currentNode->children[c-'a'];
+        }
+        currentNode->flag = true;
     }
     bool search(string word) {
+        TrieNode* currentNode = root;
+        for (char c: word) {
+            if (currentNode->children[c-'a'] != nullptr) {
+                currentNode = currentNode->children[c-'a'];
+            }
+            else {
+                return false;
+            }
+        }
+        return (currentNode->flag);
     }
     bool startsWith(string prefix) {
+        TrieNode* currentNode = root;
+        for (char c: prefix) {
+            if (currentNode->children[c-'a'] != nullptr) {
+                currentNode = currentNode->children[c-'a'];
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
     }
 };
 
