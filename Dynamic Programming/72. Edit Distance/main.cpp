0988f3cb811c3e5a924692bd66c81288f0d5ceda
Techuang <chuangellow@gmail.com>
2024-01-22 14:46:02 +0800

    Init 72. Edit Distance

---
+++ Dynamic Programming/72. Edit Distance/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        
+    }
+};
+
+int main(void) {
+    string word1, word2;
+    cin >> word1 >> word2;
+    Solution sol;
+    cout << sol.minDistance(word1, word2) << endl;
+    return 0;
+}
