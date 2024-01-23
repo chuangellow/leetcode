0283062ee3499af6c2530f531b3418f491c061c5
Techuang <chuangellow@gmail.com>
2024-01-24 01:21:23 +0800

    Solve 72. Edit Distance in O(nm) time and O(nm) space by dynamic programming

--- Dynamic Programming/72. Edit Distance/main.cpp
+++ Dynamic Programming/72. Edit Distance/main.cpp
@@ -1,51 +1,30 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
-    int countDiff(string word1, string word2, int len) {
-        int count = 0;
-        for (int i = 0; i < len; i++) {
-            if (word1[i] != word2[i]) count++;
-        }
-        return count;
-    }
-    void getDistance(string word1, int distance, int* minDistance, string word2) {
-        if (word1.compare(word2) == 0) {
-            *minDistance = min(*minDistance, distance);
-            return;
-        }
-        int len1 = word1.length();
-        int len2 = word2.length();
-        if (len1 == len2) {
-            getDistance(word2, distance+countDiff(word1, word2, len1), minDistance, word2);
-            return;
-        }
-        else if (len1 < len2) {
-            for (int pos = 0; pos <= len1; pos++) {
-                for (auto c: word2) {
-                    string newWord = word1;
-                    newWord.insert(pos, 1, c);
-                    getDistance(newWord, distance+1, minDistance, word2);
+    int minDistance(string word1, string word2) {
+        int word1Len = word1.length();
+        int word2Len = word2.length();
+        vector<vector<int>> dp(word1Len+1, vector<int>(word2Len+1, 0));
+
+        for (int i = 0; i <= word1Len; i++) dp[i][0] = i;
+        for (int i = 0; i <= word2Len; i++) dp[0][i] = i;
+
+        for (int i = 1; i <= word1Len; i++) {
+            for (int j = 1; j <= word2Len; j++) {
+                if (word1[i-1] == word2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = min({dp[i-1][j-1], dp[i-1][j], dp[i][j-1]}) + 1;
                 }
             }
-            return;
         }
-        else {
-            for (int pos = 0; pos < len1; pos++) {
-                string newWord = word1;
-                newWord.erase(pos, 1);
-                getDistance(newWord, distance+1, minDistance, word2);
-            }
-            return;
-        }
-    }
-    int minDistance(string word1, string word2) {
-        int dist = INT32_MAX;
-        getDistance(word1, 0, &dist, word2);
-        return dist;
+        return dp[word1Len][word2Len];
     }
 };
 
