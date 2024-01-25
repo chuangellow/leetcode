518b427afb4efb9b5a4a825229b67a6a78f3f2d9
Techuang <chuangellow@gmail.com>
2024-01-25 15:22:28 +0800

    Solve 300. Longest Increasing Subsequence in O(n^2) time and O(n) space by dynamic programming

--- Dynamic Programming/300. Longest Increasing Subsequence/main.cpp
+++ Dynamic Programming/300. Longest Increasing Subsequence/main.cpp
@@ -5,34 +5,20 @@ using namespace std;
 
 class Solution {
 public:
-    bool checkIncreasing(vector<bool>& mask, vector<int>& nums, int n) {
-        int temp = INT32_MIN;
-        for (int i = 0; i < n; i++) {
-            if (!mask[i]) continue;
-            if (nums[i] <= temp) return false;
-            temp = nums[i];
-        }
-        return true;
-    }
-    void getLenOfLIS(int pos, int* maxLen, vector<bool>& mask, vector<int>& nums, int n) {
-        if (pos == n) {
-            if (!checkIncreasing(mask, nums, n)) return;
-            int len = 0;
-            for (int i = 0; i < n; i++) len += (mask[i])? 1: 0;
-            *maxLen = max(*maxLen, len);
-            return;
-        }
-        mask[pos] = 0;
-        getLenOfLIS(pos+1, maxLen, mask, nums, n);
-        mask[pos] = 1;
-        getLenOfLIS(pos+1, maxLen, mask, nums, n);
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<bool> mask(n, true);
-        int maxLen = -1;
-        getLenOfLIS(0, &maxLen, mask, nums, n);
-        return maxLen;
+        if (n == 1) return 1;
+        vector<int> dp(n, 1);
+        int finalMaxValue = 1;
+        for (int i = 0; i < n; i++) {
+            int maxValue = 0;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) maxValue = max(maxValue, dp[j]);
+            }
+            dp[i] = maxValue + 1;
+            finalMaxValue = max(finalMaxValue, dp[i]);
+        }
+        return finalMaxValue;
     }
 };
 
