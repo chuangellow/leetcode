f2e16e1d5fddcced7277ab50cbba829dc3a0c77b
Techuang <chuangellow@gmail.com>
2024-01-25 14:44:11 +0800

    Refine the solution on 712. Minimum ASCII Delete Sum for Two Strings to reduce the memory complexity by 1D table

--- Dynamic Programming/712. Minimum ASCII Delete Sum for Two Strings/main.cpp
+++ Dynamic Programming/712. Minimum ASCII Delete Sum for Two Strings/main.cpp
@@ -9,18 +9,21 @@ public:
     int minimumDeleteSum(string s1, string s2) {
         int len1 = s1.length();
         int len2 = s2.length();
-        vector<vector<int>> dp(len1+1, vector<int>(len2+1, 0));
-        for (int i = 1; i <= len1; i++) dp[i][0] = dp[i-1][0] + int(s1[i-1]);
-        for (int j = 1; j <= len2; j++) dp[0][j] = dp[0][j-1] + int(s2[j-1]);
-        if (len1 == 0) return dp[0][len2];
-        if (len2 == 0) return dp[len1][0];
+        vector<int> prevRow(len2+1, 0);
+        for (int j = 1; j <= len2; j++) prevRow[j] = prevRow[j-1] + int(s2[j-1]);
+        if (len1 == 0) return prevRow[len2];
+        vector<int> currentRow(len2+1, 0);
+        int temp = 0;
         for (int i = 1; i <= len1; i++) {
+            temp += int(s1[i-1]);
+            currentRow[0] = temp;
             for (int j = 1; j <= len2; j++) {
-                if (s1[i-1] == s2[j-1]) dp[i][j] = dp[i-1][j-1];
-                else dp[i][j] = min(dp[i-1][j] + int(s1[i-1]), dp[i][j-1] + int(s2[j-1]));
+                if (s1[i-1] == s2[j-1]) currentRow[j] = prevRow[j-1];
+                else currentRow[j] = min(prevRow[j] + int(s1[i-1]), currentRow[j-1] + int(s2[j-1]));
             }
+            for (int j = 0; j <= len2; j++) prevRow[j] = currentRow[j];
         }
-        return dp[len1][len2];
+        return currentRow[len2];
     }
 };
 
