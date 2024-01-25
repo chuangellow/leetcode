9271d7e4404b421b455fe55478ea302854d3a9c7
Techuang <chuangellow@gmail.com>
2024-01-26 01:07:18 +0800

    Solve 673. Number of Longest Increasing Subsequence in O(n^2) time and O(n) space by dynamic programming

--- Dynamic Programming/673. Number of Longest Increasing Subsequence/main.cpp
+++ Dynamic Programming/673. Number of Longest Increasing Subsequence/main.cpp
@@ -5,57 +5,33 @@ using namespace std;
 
 class Solution {
 public:
-    int binarySearch(vector<int>& subSequence, int len, int target) {
-        int left = 0, right = len-1;
-        while (left < right) {
-            int mid = ((unsigned int) left + (unsigned int) right) >> 1;
-            if (subSequence[mid] < target) left = mid + 1;
-            else right = mid;
-        }
-        return left;
-    }
-    int getLengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        int maxLen = 1;
-        vector<int> subSequence = {nums[0]};
-        int len = 1;
-        for (int i = 1; i < n; i++) {
-            if (nums[i] > subSequence[len-1]) {
-                subSequence.push_back(nums[i]);
-                len++;
-            }
-            else {
-                int pos = binarySearch(subSequence, len, nums[i]);
-                subSequence[pos] = nums[i];
-            }
-        }
-        return len;
-    }
-    void getNumberOfLIS(int pos, int* count, vector<bool>& mask, vector<int>& nums, int n, int lengthOFLIS) {
-        if (n == pos) {
-            int temp = INT32_MIN;
-            int currentLen = 0;
-            for (int i = 0; i < n; i++) {
-                if (!mask[i]) continue;
-                if (nums[i] < temp) return;
-                temp = nums[i];
-                currentLen++;
-            }
-            if (currentLen == lengthOFLIS) *count = *count + 1;
-            return;
-        }
-        mask[pos] = false;
-        getNumberOfLIS(pos+1, count, mask, nums, n, lengthOFLIS);
-        mask[pos] = true;
-        getNumberOfLIS(pos+1, count, mask, nums, n, lengthOFLIS);
-        return;
-    }
     int findNumberOfLIS(vector<int>& nums) {
         int n = nums.size();
-        int lengthOFLIS = getLengthOfLIS(nums);
-        vector<bool> mask(n, true);
+        vector<int> lengthTable(n, 1);
+        for (int i = 1; i < n; i++) {
+            int maxValue = 0;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] >= nums[i]) continue;
+                maxValue = max(lengthTable[j], maxValue);
+            }
+            lengthTable[i] = maxValue + 1;
+        }
+        vector<int> numberTable(n, 1);
+        for (int i = 1; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] >= nums[i]) continue;
+                if (lengthTable[j] == lengthTable[i] - 1) count += numberTable[j];
+            }
+            if (lengthTable[i] - 1 == 0) numberTable[i] = 1;
+            else numberTable[i] = count;
+        }
+        int lenOfLIS = 0;
+        for (int i = 0; i < n; i++) lenOfLIS = max(lenOfLIS, lengthTable[i]);
         int count = 0;
-        getNumberOfLIS(0, &count, mask, nums, n, lengthOFLIS);
+        for (int i = 0; i < n; i++) {
+            if (lengthTable[i] == lenOfLIS) count += numberTable[i];
+        }
         return count;
     }
 };
