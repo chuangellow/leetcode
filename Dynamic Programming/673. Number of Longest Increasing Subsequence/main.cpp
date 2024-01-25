2816e8b33d2df2e5678c3e8a8d1e4f3bf4e98736
Techuang <chuangellow@gmail.com>
2024-01-25 23:48:20 +0800

    Init 673. Number of Longest Increasing Subsequence

---
+++ Dynamic Programming/673. Number of Longest Increasing Subsequence/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        
+    }
+};
+
+int main(void) {
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) cin >> nums[i];
+    Solution sol;
+    cout << sol.findNumberOfLIS(nums) << endl;
+    return 0;
+}
