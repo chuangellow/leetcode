21ec95f864948e5d7bbfccba29960923992b28a8
Techuang <chuangellow@gmail.com>
2024-01-26 15:32:31 +0800

    Solve 646. Maximum Length of Pair Chain in O(n^2) time and O(n) space by greedy method

--- Dynamic Programming/646. Maximum Length of Pair Chain/main.cpp
+++ Dynamic Programming/646. Maximum Length of Pair Chain/main.cpp
@@ -6,23 +6,30 @@ using namespace std;
 
 class Solution {
 public:
-    int findLongestChain(vector<vector<int>>& pairs) {
-        sort(pairs.begin(), pairs.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[1] < b[1];
-        });
-        int n = pairs.size();
-        vector<int> dp(n, 1);
-        int finalMaxValue = 1;
-        for (int i = 1; i < n; i++) {
-            int maxValue = -1;
-            for (int j = 0; j < i; j++) {
-                if (pairs[i][0] <= pairs[j][1]) continue;
-                maxValue = max(maxValue, dp[j]);
-            }
-            dp[i] = maxValue + 1;
-            finalMaxValue = max(finalMaxValue, dp[i]);
+    int linearSearch(vector<int>& secondNums, int targetLeft, int targetRight, int n) {
+        for (int i = 0; i < n; i++) {
+            if (secondNums[i] >= targetLeft && secondNums[i] > targetRight) return i;
         }
-        return finalMaxValue;
+        return -1;
+    }
+    int findLongestChain(vector<vector<int>>& pairs) {
+        int n = pairs.size();
+        sort(pairs.begin(), pairs.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[0] < b[0];
+        });
+        vector<int> secondNums = {pairs[0][1]};
+        int len = 1;
+        for (int i = 1; i < n; i++) {
+            if (pairs[i][0] > secondNums[len-1]) {
+                secondNums.push_back(pairs[i][1]);
+                len++;
+            }
+            else {
+                int idx = linearSearch(secondNums, pairs[i][0], pairs[i][1], len);
+                if (idx != -1) secondNums[idx] = pairs[i][1];
+            }
+        }
+        return len;
     }
 };
 
