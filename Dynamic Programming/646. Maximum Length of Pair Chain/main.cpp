ad2fcbd9577f165dda4553bcb44ec8d711510939
Techuang <chuangellow@gmail.com>
2024-01-26 16:24:54 +0800

    Solve 646. Maximum Length of Pair Chain in O(nlogn) time and O(n) space by another greedy method

--- Dynamic Programming/646. Maximum Length of Pair Chain/main.cpp
+++ Dynamic Programming/646. Maximum Length of Pair Chain/main.cpp
@@ -6,40 +6,17 @@ using namespace std;
 
 class Solution {
 public:
-    int binartSearch(vector<int>& secondNums, int targetLeft, int targetRight, int n) {
-        int left = 0, right = n - 1;
-        while (left <= right) {
-            int mid = ((unsigned int) left + (unsigned int) right) >> 1;
-            if (secondNums[mid] < targetLeft) left = mid + 1;
-            else {
-                if (targetRight < secondNums[mid]) right = mid;
-                else left = mid + 1;
-            }
-        }
-        if (left == n) return -1;
-        return (secondNums[left] >= targetLeft && secondNums[left] > targetRight)? left: -1;
-    }
-    int linearSearch(vector<int>& secondNums, int targetLeft, int targetRight, int n) {
-        for (int i = 0; i < n; i++) {
-            if (secondNums[i] >= targetLeft && secondNums[i] > targetRight) return i;
-        }
-        return -1;
-    }
     int findLongestChain(vector<vector<int>>& pairs) {
         int n = pairs.size();
         sort(pairs.begin(), pairs.end(), [](const vector<int>& a, const vector<int>& b) {
             return a[1] < b[1];
         });
-        vector<int> secondNums = {pairs[0][1]};
+        int prevRight = pairs[0][1];
         int len = 1;
         for (int i = 1; i < n; i++) {
-            if (pairs[i][0] > secondNums[len-1]) {
-                secondNums.push_back(pairs[i][1]);
+            if (pairs[i][0] > prevRight) {
                 len++;
-            }
-            else {
-                int idx = binartSearch(secondNums, pairs[i][0], pairs[i][1], len);
-                if (idx != -1) secondNums[idx] = pairs[i][1];
+                prevRight = pairs[i][1];
             }
         }
         return len;
