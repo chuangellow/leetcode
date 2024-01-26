8ae65d14aa47afe8916c4ec502e14ac4080f279b
Techuang <chuangellow@gmail.com>
2024-01-26 14:45:22 +0800

    Solve 646. Maximum Length of Pair Chain in O(n^2) time and O(n) space by dynamic programming

--- Dynamic Programming/646. Maximum Length of Pair Chain/main.cpp
+++ Dynamic Programming/646. Maximum Length of Pair Chain/main.cpp
@@ -1,39 +1,28 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
-    void getLenOfLongestChain(int len, int* maxLen, vector<int>& chain, vector<bool>& selected, vector<vector<int>>& pairs, int n) {
-        bool flag = false;
-        for (int i = 0; i < n; i++) {
-           if (selected[i]) continue;
-           if (pairs[i][0] > pairs[chain[len-1]][1]) {
-            flag = true;
-            chain.push_back(i);
-            selected[i] = true;
-            getLenOfLongestChain(len+1, maxLen, chain, selected, pairs, n);
-            selected[i] = false;
-            chain.pop_back();
-           }
-        }
-        if (!flag) *maxLen = max(*maxLen, len);
-        return;
-    }
     int findLongestChain(vector<vector<int>>& pairs) {
+        sort(pairs.begin(), pairs.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[1] < b[1];
+        });
         int n = pairs.size();
-        vector<bool> selected(n, false);
-        vector<int> chain;
-        int maxLen = -1;
-        for (int i = 0; i < n; i++) {
-            chain.push_back(i);
-            selected[i] = true;
-            getLenOfLongestChain(1, &maxLen, chain, selected, pairs, n);
-            selected[i] = false;
-            chain.pop_back();
+        vector<int> dp(n, 1);
+        int finalMaxValue = 1;
+        for (int i = 1; i < n; i++) {
+            int maxValue = -1;
+            for (int j = 0; j < i; j++) {
+                if (pairs[i][0] <= pairs[j][1]) continue;
+                maxValue = max(maxValue, dp[j]);
+            }
+            dp[i] = maxValue + 1;
+            finalMaxValue = max(finalMaxValue, dp[i]);
         }
-        return maxLen;
+        return finalMaxValue;
     }
 };
 
