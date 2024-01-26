dc2b56d39b228ee480a05b8c930959d8a50b5b7d
Techuang <chuangellow@gmail.com>
2024-01-26 16:52:35 +0800

    Solve 1218. Longest Arithmetic Subsequence of Given Difference in O(n^2) time and O(n) space by dynamic programming method

--- Dynamic Programming/1218. Longest Arithmetic Subsequence of Given Difference/main.cpp
+++ Dynamic Programming/1218. Longest Arithmetic Subsequence of Given Difference/main.cpp
@@ -6,7 +6,19 @@ using namespace std;
 class Solution {
 public:
     int longestSubsequence(vector<int>& arr, int difference) {
-        
+        int n = arr.size();
+        vector<int> dp(n, 1);
+        int finalMaxValue = 1;
+        for (int i = 1; i < n; i++) {
+            int maxValue = 0;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] - arr[j] != difference) continue;
+                maxValue = max(dp[j], maxValue);
+            }
+            dp[i] = maxValue + 1;
+            finalMaxValue = max(finalMaxValue, dp[i]);
+        }
+        return finalMaxValue;
     }
 };
 
