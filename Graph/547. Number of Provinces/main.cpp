7a23442d5b71282cbab1763f5fa5a5758e82166c
Techuang <chuangellow@gmail.com>
2024-01-27 17:43:51 +0800

    Solve 547. Number of Provinces in O(|V|^2+O(|V||E|)) time and O(|V|) space by union-find and disjoint-set with the disjoint-set forest implementation

--- Graph/547. Number of Provinces/main.cpp
+++ Graph/547. Number of Provinces/main.cpp
@@ -4,77 +4,44 @@
 
 using namespace std;
 
-class Set;
-
 class Node {
 public:
     int data;
-    shared_ptr<Node> next;
-    shared_ptr<Set> representative;
-    Node(int data, shared_ptr<Node> next, shared_ptr<Set> representative)
-        : data(data), next(next), representative(representative) {}
-};
-
-class Set {
-public:
-    int len;
-    shared_ptr<Node> head;
-    shared_ptr<Node> tail;
-    Set(shared_ptr<Node> head, shared_ptr<Node> tail)
-        : head(head), tail(tail) {}
-    void traverseSet() {
-        if (len == 0) return;
-        shared_ptr<Node> currentNode = head;
-        while (currentNode != nullptr) {
-            cout << currentNode->data << " ";
-            currentNode = currentNode->next;
-        }
-        cout << endl;
-        return;
-    }
+    int rank;
+    shared_ptr<Node> parent;
+    Node(int data, int rank, shared_ptr<Node> parent): data(data), rank(rank), parent(parent) {}
 };
 
 class DisjointSet {
+private:
+    void linkSet(shared_ptr<Node> node1, shared_ptr<Node> node2) {
+        if (node1->rank > node2->rank) {
+            node2->parent = node1;
+        }
+        else {
+            node1->parent = node2;
+            if (node1->rank == node2->rank) node2->rank += 1;
+        }
+        return;
+    }
 public:
     int numberOfSet = 0;
     shared_ptr<Node> makeSet(int data) {
-        shared_ptr<Node> newNode = make_shared<Node>(data, nullptr, nullptr);
-        shared_ptr<Set> newSet = make_shared<Set>(newNode, newNode);
-        newNode->representative = newSet;
-        newSet->len = 1;
+        shared_ptr<Node> newNode = make_shared<Node>(data, 0, nullptr);
+        newNode->parent = newNode;
         numberOfSet += 1;
         return newNode;
     }
-    void unionSet(shared_ptr<Node> node1, shared_ptr<Node> node2) {
-        if (node1 == nullptr || node2 == nullptr) return;
-        shared_ptr<Set> s1 = node1->representative;
-        shared_ptr<Set> s2 = node2->representative;
-        if (s1->len >= s2->len) {
-            s1->len = s1->len + s2->len;
-            s1->tail->next = s2->head;
-            shared_ptr<Node> currentNode = s2->head;
-            while (currentNode != nullptr) {
-                currentNode->representative = s1;
-                currentNode = currentNode->next;
-            }
-            s1->tail = s2->tail;
-        }
-        else {
-            s2->len = s1->len + s2->len;
-            s2->tail->next = s1->head;
-            shared_ptr<Node> currentNode = s1->head;
-            while (currentNode != nullptr) {
-                currentNode->representative = s2;
-                currentNode = currentNode->next;
-            }
-            s2->tail = s1->tail;
-        }
-        numberOfSet -= 1;
-        return;
-    }
     shared_ptr<Node> findSet(shared_ptr<Node> node) {
         if (node == nullptr) return nullptr;
-        return node->representative->head;
+        if (node->parent != node) {
+            node->parent = findSet(node->parent);
+        }
+        return node->parent;
+    }
+    void unionSet(shared_ptr<Node> node1, shared_ptr<Node> node2) {
+        linkSet(findSet(node1), findSet(node2));
+        numberOfSet -= 1;
     }
 };
 
