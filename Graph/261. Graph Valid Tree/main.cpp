513d04e17b624b86320449c35bba7e0fe01f2d42
Techuang <chuangellow@gmail.com>
2024-01-28 14:16:13 +0800

    Solve 261. Graph Valid Tree in O(|V| + |E|) time and O(|V| + |E|) space by DFS

--- Graph/261. Graph Valid Tree/main.cpp
+++ Graph/261. Graph Valid Tree/main.cpp
@@ -3,66 +3,124 @@
 
 using namespace std;
 
-class SetNode {
+class Node {
 public:
     int data;
-    int rank;
-    shared_ptr<SetNode> parent;
-    SetNode(int data, int rank, shared_ptr<SetNode> parent): data(data), rank(rank), parent(parent) {}
+    shared_ptr<Node> next;
+    Node(int data, shared_ptr<Node> next): data(data), next(next) {}
 };
 
-class DisjointSet {
-private:
-    void linkSet(shared_ptr<SetNode> node1, shared_ptr<SetNode> node2) {
-        if (node1->rank > node2->rank) {
-            node2->parent = node1;
+class LinkedList {
+public:
+    shared_ptr<Node> head;
+    shared_ptr<Node> tail;
+    int len;
+    LinkedList(): tail(nullptr), len(0) {
+        shared_ptr<Node> pseudoHead = make_shared<Node>(-1, nullptr);
+        this->head = pseudoHead;
+    }
+    LinkedList(shared_ptr<Node> node): tail(node), len(1) {
+        shared_ptr<Node> pseudoHead = make_shared<Node>(-1, node);
+        this->head = pseudoHead;
+    }
+    void insertToTail(int data) {
+        if (head == nullptr || tail == nullptr) {
+            shared_ptr<Node> newNode = make_shared<Node>(data, nullptr);
+            shared_ptr<Node> pseudoHead = make_shared<Node>(-1, newNode);
+            head = pseudoHead;
+            tail = newNode;
+            len = 1;
+            return;
         }
-        else {
-            node1->parent = node2;
-            if (node1->rank == node2->rank) node2->rank += 1;
-        }
+        shared_ptr<Node> newNode = make_shared<Node>(data, nullptr);
+        tail->next = newNode;
+        tail = newNode;
+        len += 1;
         return;
     }
-public:
-    int numOfSets = 0;
-    shared_ptr<SetNode> makeSet(int data) {
-        shared_ptr<SetNode> newNode = make_shared<SetNode>(data, 0, nullptr);
-        newNode->parent = newNode;
-        numOfSets += 1;
-        return newNode;
-    }
-    shared_ptr<SetNode> findSet(shared_ptr<SetNode> node) {
-        if (node == nullptr) return nullptr;
-        if (node->parent != node) {
-            node->parent = findSet(node->parent);
+    void traverse() {
+        if (head == nullptr || tail == nullptr) return;
+        shared_ptr<Node> currentNode = head->next;
+        while (currentNode != nullptr) {
+            cout << currentNode->data << " ";
+            currentNode = currentNode->next;
         }
-        return node->parent;
+        cout << endl;
+        return;
     }
-    void unionSet(shared_ptr<SetNode> node1, shared_ptr<SetNode> node2) {
-        linkSet(findSet(node1), findSet(node2));
-        numOfSets -= 1;
+    bool findData(int data) {
+        if (head == nullptr || tail == nullptr) return false;
+        shared_ptr<Node> currentNode = head->next;
+        while (currentNode != nullptr) {
+            if (currentNode->data == data) return true;
+            currentNode = currentNode->next;
+        }
+        return false;
+    }
+};
+
+class Graph {
+public:
+    vector<shared_ptr<LinkedList>> lists;
+    Graph(vector<vector<int>>& adjacencyMatrix) {
+        int n = adjacencyMatrix.size();
+        for (int i = 0; i < n; i++) {
+            shared_ptr<LinkedList> list = make_shared<LinkedList>();
+            for (int j = 0; j < n; j++) {
+                if (adjacencyMatrix[i][j]) list->insertToTail(j);
+            }
+            lists.push_back(list);
+        }
+    }
+    Graph(int numNodes, vector<vector<int>>& edges, bool isDirected) {
+        for (int i = 0; i < numNodes; i++) {
+            shared_ptr<LinkedList> list = make_shared<LinkedList>();
+            lists.push_back(list);
+        }
+        for (auto edge: edges) {
+            lists[edge[0]]->insertToTail(edge[1]);
+            if (!(isDirected)) lists[edge[1]]->insertToTail(edge[0]);
+        }
     }
 };
 
 class Solution {
 public:
-    bool checkConnected(vector<vector<int>>& edges, int n) {
-        shared_ptr<DisjointSet> newDisjointSet = make_shared<DisjointSet>();
-        vector<shared_ptr<SetNode>> nodes;
-        for (int i = 0; i < n; i++) {
-            shared_ptr<SetNode> newNode = newDisjointSet->makeSet(i);
-            nodes.push_back(newNode);
+    void DFSVisit(int sourceIdx, shared_ptr<Graph> graph, int n, vector<bool>& visited) {
+        shared_ptr<LinkedList> edges = graph->lists[sourceIdx];
+        shared_ptr<Node> currentEdge = edges->head->next;
+        visited[sourceIdx] = true;
+        while (currentEdge != nullptr) {
+            int nextIdx = currentEdge->data;
+            if (nextIdx == sourceIdx) {
+                currentEdge = currentEdge->next;
+                continue;
+            }
+            if (!visited[nextIdx]) {
+                DFSVisit(nextIdx, graph, n, visited);
+            }
+            currentEdge = currentEdge->next;
         }
-        for (auto& edge: edges) {
-            if (newDisjointSet->findSet(nodes[edge[0]]) != newDisjointSet->findSet(nodes[edge[1]])) {
-                newDisjointSet->unionSet(nodes[edge[0]], nodes[edge[1]]);
+        return;
+    }
+    bool DFS(shared_ptr<Graph> graph, int n) {
+        vector<bool> visited(n, false);
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                DFSVisit(i, graph, n, visited);
+                count++;
             }
         }
-        return (newDisjointSet->numOfSets == 1)? true: false;
+        return (count == 1)? true: false;
+    }
+    bool checkConnected(int n, vector<vector<int>>& edges) {
+        shared_ptr<Graph> graph = make_shared<Graph>(n, edges, false);
+        return DFS(graph, n);
     }
     bool validTree(int n, vector<vector<int>>& edges) {
         int m = edges.size();
-        return ((m == n - 1) && checkConnected(edges, n));
+        return ((m == n - 1) && checkConnected(n, edges));
     }
 };
 
