d561b3fb917878f60c62bd96237da1f81604ff45
Techuang <chuangellow@gmail.com>
2024-02-01 14:19:16 +0800

    Refine the parser in implementation of the singly linked list to make it more scalable and cleaner

--- Linked List/Implementations/Singly Linked List/main.cpp
+++ Linked List/Implementations/Singly Linked List/main.cpp
@@ -2,38 +2,47 @@
 #include <string>
 #include <iostream>
 #include <memory>
+#include <unordered_map>
+#include <functional>
 #include "LinkedList.h"
 
 void parseInput(std::shared_ptr<LinkedList> list, int numCommands) {
+    std::unordered_map<std::string, std::function<void()>> commandMap;
+
+    commandMap["insertToHead"] = [&]() {
+        int data;
+        std::cin >> data;
+        list->insertToHead(data);
+    };
+    commandMap["insertToTail"] = [&]() {
+        int data;
+        std::cin >> data;
+        list->insertToTail(data);
+    };
+    commandMap["removeHead"] = [&]() {
+        list->removeHead();
+    };
+    commandMap["removeTail"] = [&]() {
+        list->removeTail();
+    };
+    commandMap["traverse"] = [&]() {
+        list->traverse();
+    };
+    commandMap["insertAfterIdx"] = [&]() {
+        int data, index;
+        std::cin >> data >> index;
+        list->insertAfterIdx(data, index);
+    };
+
     for (int i = 0; i < numCommands; i++) {
         std::string command;
         std::cin >> command;
-        if (command.compare("insertToHead") == 0) {
-            int data;
-            std::cin >> data;
-            list->insertToHead(data);
-        }
-        else if (command.compare("insertToTail") == 0) {
-            int data;
-            std::cin >> data;
-            list->insertToTail(data);
-        }
-        else if (command.compare("removeHead") == 0) {
-            list->removeHead();
-        }
-        else if (command.compare("removeTail") == 0) {
-            list->removeTail();
-        }
-        else if (command.compare("traverse") == 0) {
-            list->traverse();
-        }
-        else if (command.compare("insertAfterIdx") == 0) {
-            int data, index;
-            std::cin >> data >> index;
-            list->insertAfterIdx(data, index);
-        }
+        auto it = commandMap.find(command);
+        if (it != commandMap.end()) {
+            it->second();
+        } 
         else {
-            std::cout << "Invalid commands!!!" << std::endl;
+            std::cout << "Invalid command!!!" << std::endl;
         }
     }
 }
