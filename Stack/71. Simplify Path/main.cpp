b0424af017f5cb5e5523e3cccece8fe506df0fa1
Techuang <chuangellow@gmail.com>
2024-03-13 17:24:45 +0800

    Refine the solution on 71. Simplify Path for simplying the code structure

--- Stack/71. Simplify Path/main.cpp
+++ Stack/71. Simplify Path/main.cpp
@@ -7,72 +7,44 @@ using namespace std;
 
 class Solution {
 public:
-    int traverse(string path, char target, int start, int size) {
-        int idx = start+1;
-        int len = 1;
-        for (; idx < size; idx++) {
-            if (path.at(idx) != target) return len;
-            len++;
-        }
-        return len;
-    }
     string simplifyPath(string path) {
-        string outPath;
-        stack<char> myStack;
-        myStack.push(path.at(0));
-        int size = path.length();
-        int index = 1;
-        int len;
-        int count;
-        while (index < size) {
-            switch (path.at(index)) {
-                case '/':
-                    len = traverse(path, '/', index, size);
-                    if (myStack.top() == '/') {
-                        index = index + len;
-                        continue;
-                    }
-                    myStack.push('/');
-                    index = index + len;
-                    break;
-                case '.':
-                    len = traverse(path, '.', index, size);
-                    if (len <= 2) {
-                        if (path.at(index-1) != '/' || index + len < size && path.at(index + len) != '/') {
-                            for (int i = 0; i < len; i++) myStack.push('.');
-                            index = index + len;
-                            break;
-                        }
-                        count = 0;
-                        while (!myStack.empty() && count < len) {
-                            if (myStack.top() == '/') count++;
-                            myStack.pop();
-                        }
-                        if (myStack.empty()) myStack.push('/');
-                    }
-                    else {
-                        for (int i = 0; i < len; i++) myStack.push('.');
-                    }
-                    index = index + len;
-                    break;
-                default:
-                    myStack.push(path.at(index));
-                    index++;
-                    break;
+        string outputStr;
+        string delim = "/";
+        size_t pos;
+        string token;
+        stack<string> myStack;
+        while ((pos = path.find(delim)) != string::npos) {
+            token = path.substr(0, pos);
+            path.erase(0, pos+delim.length());
+            if (token.compare("") == 0) continue;
+            if (token.compare(".") == 0) continue;
+            if (token.compare("..") == 0) {
+                if (!myStack.empty()) myStack.pop();
+                continue;
+            }
+            myStack.push(token);
+        }
+        token = path.substr(0, string::npos);
+        if (token.compare(".") != 0 && token.compare("") != 0) {
+            if (token.compare("..") == 0) {
+                if (!myStack.empty()) myStack.pop();
+            }
+            else {
+                myStack.push(token);
             }
         }
-        if (myStack.top() == '/') myStack.pop();
-        if (myStack.empty()) myStack.push('/');
-        stack<char> outStack;
+        stack<string> outStack;
         while (!myStack.empty()) {
             outStack.push(myStack.top());
             myStack.pop();
         }
         while (!outStack.empty()) {
-            outPath.push_back(outStack.top());
+            outputStr.push_back('/');
+            outputStr.append(outStack.top());
             outStack.pop();
         }
-        return outPath;
+        if (outputStr.empty()) outputStr.push_back('/');
+        return outputStr;
     }
 };
 
