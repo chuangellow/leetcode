dbe3dd77157fcbb44abe7c38e9d67caba4092d52
Techuang <chuangellow@gmail.com>
2024-03-14 01:16:03 +0800

    Solve 1249. Minimum Remove to Make Valid Parentheses

--- Stack/1249. Minimum Remove to Make Valid Parentheses/main.cpp
+++ Stack/1249. Minimum Remove to Make Valid Parentheses/main.cpp
@@ -1,12 +1,69 @@
 #include <iostream>
+#include <stack>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        
+        string::size_type size = s.length();
+        string outStr;
+        stack<char> myStack;
+        vector<int> invalidLeft;
+        vector<int> invalidRight;
+        for (int i = 0; i < size; i++) {
+            switch (s.at(i)) {
+                case ')':
+                    if (myStack.empty()) invalidRight.push_back(i);
+                    else myStack.pop();
+                    break;
+                case '(':
+                    myStack.push('(');
+                    break;
+                default:
+                    break;
+            }
+        }
+        while (!myStack.empty()) myStack.pop();
+        for (int i = size-1; i >= 0; i--) {
+            switch (s.at(i)) {
+                case '(':
+                    if (myStack.empty()) invalidLeft.push_back(i);
+                    else myStack.pop();
+                    break;
+                case ')':
+                    myStack.push(')');
+                    break;
+                default:
+                    break;
+            }
+        }
+        int lsize = invalidLeft.size(), rsize = invalidRight.size();
+        int lCount = lsize-1, rCount = 0;
+        for (int i = 0; i < size; i++) {
+            switch (s[i]) {
+                case ')':
+                    if (rCount < rsize && i == invalidRight[rCount]) {
+                        rCount++;
+                        continue;
+                    }
+                    outStr.push_back(')');
+                    break;
+                case '(':
+                    if (lCount >= 0 && i == invalidLeft[lCount]) {
+                        lCount--;
+                        continue;
+                    }
+                    outStr.push_back('(');
+                    break;
+                default:
+                    outStr.push_back(s[i]);
+                    break;
+            }
+        }
+        return outStr;
     }
 };
 
