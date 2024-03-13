0a0b9ee376df2a63fa211cb7803afad895ddb0ea
Techuang <chuangellow@gmail.com>
2024-03-14 00:25:18 +0800

    Init 1249. Minimum Remove to Make Valid Parentheses

---
+++ Stack/1249. Minimum Remove to Make Valid Parentheses/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        
+    }
+};
+
+int main(void) {
+    string s;
+    cin >> s;
+    Solution sol;
+    cout << sol.minRemoveToMakeValid(s) << endl;
+}
