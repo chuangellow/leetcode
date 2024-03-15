0939187c375c5f43630b88031d28524acb7a5fba
Techuang <chuangellow@gmail.com>
2024-03-16 00:16:07 +0800

    Solve 227. Basic Calculator II by stack

--- Stack/227. Basic Calculator II/main.cpp
+++ Stack/227. Basic Calculator II/main.cpp
@@ -7,28 +7,65 @@ using namespace std;
 
 class Solution {
 public:
-    int strToInt(string s) {
-        int val;
-        stringstream ss;
-        ss << s;
-        ss >> val;
-        return val;
-    }
-    string intToStr(int val) {
-        string out;
-        stringstream ss;
-        ss << val;
-        ss >> out;
-        return out;
-    }
     int calculate(string s) {
-        return 0;
+        // ensure that after parsing the s, the sum of the lValueStack is the result.
+        // we do the last operation until we encounter the next operation.
+        size_t size = s.length();
+        int rValue = 0;
+        stack<int> lValueStack;
+        char lastOp = '+';
+        for (int i = 0; i < size; i++) {
+            char c = s[i];
+            if (isdigit(c)) rValue = rValue * 10 + (c - '0');
+            else if (!iswspace(c)) {
+                if (lastOp == '+') {
+                    lValueStack.push(rValue);
+                }
+                else if (lastOp == '-') {
+                    lValueStack.push(-rValue);
+                }
+                else if (lastOp == '*') {
+                    int lValue = lValueStack.top();
+                    lValueStack.pop();
+                    lValueStack.push(lValue * rValue);
+                }
+                else if (lastOp == '/') {
+                    int lValue = lValueStack.top();
+                    lValueStack.pop();
+                    lValueStack.push(lValue / rValue);
+                }
+                lastOp = c;
+                rValue = 0;
+            }
+        }
+        if (lastOp == '+') {
+            lValueStack.push(rValue);
+        }
+        else if (lastOp == '-') {
+            lValueStack.push(-rValue);
+        }
+        else if (lastOp == '*') {
+            int lValue = lValueStack.top();
+            lValueStack.pop();
+            lValueStack.push(lValue * rValue);
+        }
+        else if (lastOp == '/') {
+            int lValue = lValueStack.top();
+            lValueStack.pop();
+            lValueStack.push(lValue / rValue);
+        }
+        int result = 0;
+        while (!lValueStack.empty()) {
+            result += lValueStack.top();
+            lValueStack.pop();
+        }
+        return result;
     }
 };
 
 int main(void) {
     string s;
-    cin >> s;
+    getline(cin, s);
     Solution sol;
     cout << sol.calculate(s) << endl;
     return 0;
