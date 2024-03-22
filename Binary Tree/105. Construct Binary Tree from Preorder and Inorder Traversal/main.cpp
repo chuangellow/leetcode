cc0cb58f5a9ae9e9f5a91b67ecb9af017c68e991
Techuang <chuangellow@gmail.com>
2024-03-22 22:42:42 +0800

    Solve 105. Construct Binary Tree from Preorder and Inorder Traversal in O(n) time

--- Binary Tree/105. Construct Binary Tree from Preorder and Inorder Traversal/main.cpp
+++ Binary Tree/105. Construct Binary Tree from Preorder and Inorder Traversal/main.cpp
@@ -17,14 +17,44 @@ struct TreeNode {
 class Solution {
 public:
     void showTree(TreeNode* node) {
-        if (!node) return;
+        if (!node) {
+            cout << "null ";
+            return;
+        }
         cout << node->val << " ";
         showTree(node->left);
         showTree(node->right);
     }
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        TreeNode* root = new TreeNode(preorder[0]);
+    int findInorder(vector<int>& inorder, int target, const int size) {
+        for (int i = 0; i <= size-1; i++) if (inorder[i] == target) return i;
+        return -1;
+    }
+    TreeNode* recursiveBuild(int *pos, int left, int right, vector<int>& preorder, vector<int>& inorder, const int size) {
+        cout << "pos: " << *pos << "left: " << left << "right: " << right << endl;
+        if (*pos >= size) return nullptr;
+        if (left > right) return nullptr;
+        TreeNode* root = new TreeNode(preorder[*pos]);
+        if (left == right) return root;
+        int rootIdx = findInorder(inorder, preorder[*pos], size);
+        *pos = *pos + 1;
+        if (*pos < size && findInorder(inorder, preorder[*pos], size) < rootIdx) {
+            root->left = recursiveBuild(pos, left, rootIdx-1, preorder, inorder, size);
+            *pos = *pos + 1;
+            root->right = recursiveBuild(pos, rootIdx+1, right, preorder, inorder, size);
+            if (root->left == nullptr || root->right == nullptr) {
+                *pos = *pos - 1;
+            }
+        }
+        else {
+            root->right = recursiveBuild(pos, rootIdx+1, right, preorder, inorder, size);
+            if (root->right == nullptr) *pos = *pos - 1;
+        }
         return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int size = preorder.size();
+        int pos = 0;
+        return recursiveBuild(&pos, 0, size-1, preorder, inorder, size);
     }
     void freeNode(TreeNode* root) {
         if (root == nullptr) return;
