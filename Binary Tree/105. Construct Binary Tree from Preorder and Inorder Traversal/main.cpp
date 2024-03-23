82f6f8dfafe7f0b25baf9611204ae08159bc795e
Techuang <chuangellow@gmail.com>
2024-03-23 14:36:53 +0800

    Refine the solution on 105. Construct Binary Tree from Preorder and Inorder Traversal by using hash map to find the index

--- Binary Tree/105. Construct Binary Tree from Preorder and Inorder Traversal/main.cpp
+++ Binary Tree/105. Construct Binary Tree from Preorder and Inorder Traversal/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unordered_set>
+#include <unordered_map>
 #include <vector>
 #include <string>
 
@@ -29,24 +30,23 @@ public:
         for (int i = 0; i <= size-1; i++) if (inorder[i] == target) return i;
         return -1;
     }
-    TreeNode* recursiveBuild(int *pos, int left, int right, vector<int>& preorder, vector<int>& inorder, const int size) {
-        cout << "pos: " << *pos << "left: " << left << "right: " << right << endl;
+    TreeNode* recursiveBuild(int *pos, int left, int right, vector<int>& preorder, unordered_map<int, int>& hashMap, const int size) {
         if (*pos >= size) return nullptr;
         if (left > right) return nullptr;
         TreeNode* root = new TreeNode(preorder[*pos]);
         if (left == right) return root;
-        int rootIdx = findInorder(inorder, preorder[*pos], size);
+        int rootIdx = hashMap[preorder[*pos]];
         *pos = *pos + 1;
-        if (*pos < size && findInorder(inorder, preorder[*pos], size) < rootIdx) {
-            root->left = recursiveBuild(pos, left, rootIdx-1, preorder, inorder, size);
+        if (*pos < size && hashMap[preorder[*pos]] < rootIdx) {
+            root->left = recursiveBuild(pos, left, rootIdx-1, preorder, hashMap, size);
             *pos = *pos + 1;
-            root->right = recursiveBuild(pos, rootIdx+1, right, preorder, inorder, size);
+            root->right = recursiveBuild(pos, rootIdx+1, right, preorder, hashMap, size);
             if (root->left == nullptr || root->right == nullptr) {
                 *pos = *pos - 1;
             }
         }
         else {
-            root->right = recursiveBuild(pos, rootIdx+1, right, preorder, inorder, size);
+            root->right = recursiveBuild(pos, rootIdx+1, right, preorder, hashMap, size);
             if (root->right == nullptr) *pos = *pos - 1;
         }
         return root;
@@ -54,7 +54,11 @@ public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int size = preorder.size();
         int pos = 0;
-        return recursiveBuild(&pos, 0, size-1, preorder, inorder, size);
+        unordered_map<int, int> hashMap;
+        for (int i = 0; i < size; i++) {
+            hashMap[preorder[i]] = findInorder(inorder, preorder[i], size);
+        }
+        return recursiveBuild(&pos, 0, size-1, preorder, hashMap, size);
     }
     void freeNode(TreeNode* root) {
         if (root == nullptr) return;
