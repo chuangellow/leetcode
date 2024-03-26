9f7de00fde5657ffff811f7c950a39df43438843
Techuang <chuangellow@gmail.com>
2024-03-26 15:47:17 +0800

    Solve 222. Count Complete Tree Nodes by simple recursion method

---
+++ Binary Tree/222. Count Complete Tree Nodes/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* buildTree(const vector<string>& nodeValues) {
+    if (nodeValues.empty()) return nullptr;
+
+    TreeNode* root = new TreeNode(stoi(nodeValues[0])); // Assuming the first value is always the root and is not null
+    vector<TreeNode*> nodes{root};
+
+    for (size_t i = 1; i < nodeValues.size(); ++i) {
+        if (nodeValues[i] != "null") {
+            TreeNode* node = new TreeNode(stoi(nodeValues[i]));
+            if (i % 2 == 1) { // Odd index, left child
+                nodes[(i - 1) / 2]->left = node;
+            } else { // Even index, right child
+                nodes[(i - 2) / 2]->right = node;
+            }
+            nodes.push_back(node);
+        }
+    }
+
+    return root;
+}
+
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if (!root) return 0;
+        return countNodes(root->left) + countNodes(root->right) + 1;
+    }
+};
+
+int main(void) {
+    int numNodes;
+    cin >> numNodes;
+    vector<string> nodeValues(numNodes);
+    for (int i = 0; i < numNodes; i++) cin >> nodeValues[i];
+    TreeNode* root = buildTree(nodeValues);
+    Solution sol;
+    cout << sol.countNodes(root) << endl;
+}
