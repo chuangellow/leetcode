f2df96a1f610fdd6528d97e3c748fa968520352a
Techuang <chuangellow@gmail.com>
2024-04-03 12:56:14 +0800

    Solve 2. Reverse Linked List II in O(n) time

--- Linked List/92. Reverse Linked List II/main.cpp
+++ Linked List/92. Reverse Linked List II/main.cpp
@@ -17,25 +17,57 @@ public:
         ListNode* currentNode = head;
         while (currentNode != nullptr) {
             cout << currentNode->val << " ";
+            currentNode = currentNode->next;
         }
         cout << endl;
+        return;
+    }
+    void reverse(ListNode* leftNode, ListNode* rightNode) {
+        ListNode* prev = nullptr, *current = leftNode;
+        while (current != rightNode) {
+            ListNode* next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+        current->next = prev;
     }
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        
+        if (left == right) return head;
+        ListNode* pseudoHead = new ListNode(-1, head);
+        ListNode* prevNode, *leftNode, *rightNode, *nextNode;
+        prevNode = pseudoHead;
+        int index = 1;
+        while (prevNode != nullptr) {
+            if (index == left) break;
+            index++;
+            prevNode = prevNode->next;
+        }
+        leftNode = prevNode->next;
+        rightNode = prevNode->next;
+        while (rightNode != nullptr) {
+            if (index == right) break;
+            index++;
+            rightNode = rightNode->next;
+        }
+        nextNode = rightNode->next;
+        reverse(leftNode, rightNode);
+        prevNode->next = rightNode;
+        leftNode->next = nextNode;
+        return pseudoHead->next;
     }
 };
 
 ListNode* createList(vector<int>& nums, int n) {
-    ListNode* head;
-    head->val = nums[0];
+    ListNode* head = new ListNode(nums[0]);
     ListNode* currentNode = head;
     for (int i = 1; i < n; i++) {
-        ListNode* newNode;
-        newNode->val = nums[i];
+        ListNode* newNode = new ListNode(nums[i]);
         currentNode->next = newNode;
         currentNode = newNode;
     }
     currentNode->next = nullptr;
+    return head;
 }
 
 int main(void) {
