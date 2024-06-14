1c85bb27e593b3dc28ceb7ca3250db64fc3f703b
Techuang <chuangellow@gmail.com>
2024-06-14 13:43:40 +0800

    Solve 416. Partition Equal Subset Sum by 2D DP

--- Dynamic Programming/416. Partition Equal Subset Sum/main.cpp
+++ Dynamic Programming/416. Partition Equal Subset Sum/main.cpp
@@ -6,19 +6,29 @@ using namespace std;
 
 class Solution {
 public:
-    void solve(int pos, vector<int>& nums, int sumA, int sumB, bool *ans) {
-        if (pos == nums.size()) {
-            if (sumA == sumB) *ans = true;
-            return;
-        }
-        solve(pos+1, nums, sumA+nums[pos], sumB, ans);
-        solve(pos+1, nums, sumA, sumB+nums[pos], ans);
-        return;
-    }
     bool canPartition(vector<int>& nums) {
-        bool ans = false;
-        solve(0, nums, 0, 0, &ans);
-        return ans;
+        long long int sum = 0;
+        int n = nums.size();
+        if (n == 1) return false;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+        if (sum % 2 == 1) return false;
+        sum /= 2;
+        vector<vector<bool>> table(n, vector<bool>(sum+1, false));
+        if (nums[0] <= sum) table[0][nums[0]] = true;
+        table[0][0] = true;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < sum+1; j++) {
+                if (j >= nums[i]) {
+                    table[i][j] = table[i-1][j] || table[i-1][j-nums[i]];
+                }
+                else {
+                    table[i][j] = table[i-1][j];
+                }
+            }
+        }
+        return table[n-1][sum];
     }
 };
 
