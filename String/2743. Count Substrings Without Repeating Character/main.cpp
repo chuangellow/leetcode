766170dcc17cee64bf87310019c2d39b27978570
Techuang <chuangellow@gmail.com>
2024-06-28 21:38:04 +0800

    Refine the solution on 2743. Count Substrings Without Repeating Character with limited size hash table

--- String/2743. Count Substrings Without Repeating Character/main.cpp
+++ String/2743. Count Substrings Without Repeating Character/main.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
 class Solution {
 public:
     int numberOfSpecialSubstrings(string s) {
-        unordered_map<char, int> map;
+        int table[26] = {0};
         int left = 0, right = 0;
         int count = 0;
         while (right < s.size()) {
-            while (map.count(s[right]) != 0) {
-                map[s[left]] -= 1;
-                if (map[s[left]] == 0) {
-                    map.erase(s[left]);
-                }
+            while (table[s[right] - 'a'] != 0) {
+                table[s[left] - 'a'] -= 1;
                 left += 1;
             }
-            if (map.count(s[right]) != 0) map[s[right]] += 1;
-            else map[s[right]] = 1;
+            table[s[right] - 'a'] += 1;
             count += (right - left + 1);
             right += 1;
         }
