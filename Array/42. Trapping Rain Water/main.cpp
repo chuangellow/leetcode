cdffa7e938fd8022647b1bb3a377da39a7569e6f
Techuang <chuangellow@gmail.com>
2024-07-02 14:47:48 +0800

    Solve 42. Trapping Rain Water in O(n) time and O(n) space by memorization

--- Array/42. Trapping Rain Water/main.cpp
+++ Array/42. Trapping Rain Water/main.cpp
@@ -8,20 +8,20 @@ public:
     int trap(vector<int>& height) {
         int n = height.size();
         int totalWater = 0;
-        for (int i = 1; i < n-1; i++) {
-            int leftTrap = -1, rightTrap = -1;
-            for (int j = i - 1; j >= 0; j--) {
-                if (height[j] > height[i]) {
-                    leftTrap = max(height[j], leftTrap);
-                }
-            }
-            for (int j = i + 1; j < n; j++) {
-                if (height[j] > height[i]) {
-                    rightTrap = max(height[j], rightTrap);
-                }
-            }
-            if (leftTrap == -1 || rightTrap == -1) continue;
-            totalWater += min(leftTrap, rightTrap) - height[i];
+        vector<int> leftMax(n, 0);
+        int maxVal = 0;
+        for (int i = 0; i < n; i++) {
+            maxVal = max(maxVal, height[i]);
+            leftMax[i] = maxVal;
+        }
+        vector<int> rightMax(n, 0);
+        maxVal = 0;
+        for (int i = n-1; i >= 0; i--) {
+            maxVal = max(maxVal, height[i]);
+            rightMax[i] = maxVal;
+        }
+        for (int i = 0; i < n; i++) {
+            totalWater += min(leftMax[i], rightMax[i]) - height[i];
         }
         return totalWater;
     }
