839c5f020973097ac0e787bec08a52c836eefa36
Techuang <chuangellow@gmail.com>
2024-07-02 13:38:02 +0800

    Solve 350. Intersection of Two Arrays II in O(n1log(n1)+n2log(n2)) by sorting and two-pointers method

--- Array/350. Intersection of Two Arrays II/main.cpp
+++ Array/350. Intersection of Two Arrays II/main.cpp
@@ -8,15 +8,17 @@ class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size(), n2 = nums2.size();
-        unordered_map<int, int> table;
-        for (int i = 0; i < n1; i++) {
-            table[nums1[i]]++;
-        }
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int p1 = 0, p2 = 0;
         vector<int> result;
-        for (int i = 0; i < n2; i++) {
-            if (table.count(nums2[i]) != 0 && table[nums2[i]] > 0) {
-                result.push_back(nums2[i]);
-                table[nums2[i]]--;
+        while (p1 < n1 && p2 < n2) {
+            while (p1 < n1 && p2 < n2 && nums1[p1] < nums2[p2]) p1++;
+            while (p1 < n1 && p2 < n2 && nums2[p2] < nums1[p1]) p2++;
+            while (p1 < n1 && p2 < n2 && nums1[p1] == nums2[p2]) {
+                result.push_back(nums1[p1]);
+                p1++;
+                p2++;
             }
         }
         return result;
