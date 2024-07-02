955f17404d36ac0258a4354c212c8fa7d269c3a7
Techuang <chuangellow@gmail.com>
2024-07-02 22:13:27 +0800

    Refine the solution on 58. Length of Last Word to remove the redudant variable

--- String/58. Length of Last Word/main.cpp
+++ String/58. Length of Last Word/main.cpp
@@ -7,20 +7,17 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         int len = s.length();
-        int lastWordLen = 0;
         int currentLen = 0;
         for (int i = 0; i < len; i++) {
             if (s[i] == ' ') {
-                if (currentLen != 0) lastWordLen = currentLen;
-                currentLen = 0;
                 continue;
             }
             else {
+                if (i > 0 && s[i-1] == ' ') currentLen = 0;
                 currentLen += 1;
             }
         }
-        if (currentLen != 0) lastWordLen = currentLen;
-        return lastWordLen;
+        return currentLen;
     }
 };
 
