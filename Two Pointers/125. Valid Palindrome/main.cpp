c44f633eb26d5d36ad1dcb3087cc52e5291baa0c
Techuang <chuangellow@gmail.com>
2024-07-03 15:38:28 +0800

    Solve 125. Valid Palindrome in O(len) time and O(len) space by two-pointers method and one-pass method

--- Two Pointers/125. Valid Palindrome/main.cpp
+++ Two Pointers/125. Valid Palindrome/main.cpp
@@ -7,21 +7,13 @@ using namespace std;
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int len = s.length(), cleanLen = 0;
-        string cleanStr;
-        for (int i = 0; i < len; i++) {
-            if (isdigit(s[i])) {
-                cleanStr.push_back(s[i]);
-                cleanLen++;
-            }
-            if (isalpha(s[i])) {
-                cleanStr.push_back(tolower(s[i]));
-                cleanLen++;
-            }
-        }
-        int left = 0, right = cleanLen - 1;
+        int len = s.length();
+        int left = 0, right = len - 1;
         while (left <= right) {
-            if (cleanStr[left] != cleanStr[right]) {
+            while (left <= right && !isalnum(s[left])) left++;
+            while (right >= left && !isalnum(s[right])) right--;
+            if (left > right) return true;
+            if (tolower(s[left]) != tolower(s[right])) {
                 return false;
             }
             left++;
