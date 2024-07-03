d4724f6562e0824d35132fe8584dc408317079e4
Techuang <chuangellow@gmail.com>
2024-07-03 15:21:42 +0800

    Solve 28. Find the Index of the First Occurrence in a String in O(n + m) time by kmp alogrithm

--- String/28. Find the Index of the First Occurrence in a String/main.cpp
+++ String/28. Find the Index of the First Occurrence in a String/main.cpp
@@ -5,19 +5,45 @@ using namespace std;
 
 class Solution {
 public:
+    vector<int> calculatePrefixFunction(string s) {
+        int len = s.length();
+        vector<int> prefixFunction(len, 0);
+        int i = 1, j = 0;
+        while (i < len) {
+            if (s[i] == s[j]) {
+                prefixFunction[i] = ++j;
+                i++;
+            }
+            else {
+                if (j != 0) {
+                    j = prefixFunction[j - 1];
+                }
+                else {
+                    i++;
+                }
+            }
+        }
+        return prefixFunction;
+    }
     int strStr(string haystack, string needle) {
         int n = haystack.size();
         int m = needle.size();
+        vector<int> prefixFunction = calculatePrefixFunction(needle);
         int i = 0, j = 0;
-        for (int i = 0; i <= n - m; i++) {
-            bool isSame = true;
-            for (int j = 0; j < m; j++) {
-                if (haystack[i+j] != needle[j]) {
-                    isSame = false;
-                    break;
+        while (i < n) {
+            if (haystack[i] == needle[j]) {
+                i++;
+                j++;
+            }
+            else {
+                if (j != 0) {
+                    j = prefixFunction[j - 1];
+                }
+                else {
+                    i++;
                 }
             }
-            if (isSame) return i;
+            if (j == m) return i - m;
         }
         return -1;
     }
