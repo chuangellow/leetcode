5a42e65e6d4e5bb7c2dfa7f33f77144f8d2b1039
Techuang <chuangellow@gmail.com>
2024-07-03 13:35:24 +0800

    Solve 73. Set Matrix Zeroes in O(mn) time and O(1) space

--- Array/73. Set Matrix Zeroes/main.cpp
+++ Array/73. Set Matrix Zeroes/main.cpp
@@ -8,24 +8,44 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
-        unordered_set<int> rowSet;
-        unordered_set<int> colSet;
+        bool clearFirstRow = false;
+        bool clearFirstCol = false;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    rowSet.insert(i);
-                    colSet.insert(j);
+                    if (i == 0) {
+                        clearFirstRow = true;
+                    }
+                    if (j == 0) {
+                        clearFirstCol = true;
+                    }
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
-        for (int row: rowSet) {
-            for (int j = 0; j < n; j++) {
-                matrix[row][j] = 0;
+        for (int i = 1; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < n; j++) {
+                    matrix[i][j] = 0;
+                }
+            } 
+        }
+        for (int j = 1; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < m; i++) {
+                    matrix[i][j] = 0;
+                }
             }
         }
-        for (int col: colSet) {
+        if (clearFirstCol) {
             for (int i = 0; i < m; i++) {
-                matrix[i][col] = 0;
+                matrix[i][0] = 0;
+            }
+        }
+        if (clearFirstRow) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
             }
         }
     }
