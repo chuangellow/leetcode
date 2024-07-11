d9806072ecf61adaec4d2736c748f4839ed88d11
Techuang <chuangellow@gmail.com>
2024-07-11 22:57:00 +0800

    Solve 230. Kth Smallest Element in a BST in O(n) time and O(n) space by inorder traversal

---
+++ Binary Tree/230. Kth Smallest Element in a BST/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode *buildTree(vector<int>& nums) {
+    int n = nums.size();
+    TreeNode *root = new TreeNode(nums[0]);
+    queue<TreeNode *> q;
+    q.push(root);
+    int currentIdx = 1;
+    while (currentIdx < n) {
+        TreeNode *parent = q.front();
+        q.pop();
+        TreeNode *left = new TreeNode(nums[currentIdx++]);
+        parent->left = left;
+        q.push(left);
+        if (currentIdx >= n) break;
+        TreeNode *right = new TreeNode(nums[currentIdx++]);
+        parent->right = right;
+        q.push(right);
+    }
+    return root;
+}
+
+void preorderTraversal(TreeNode *root) {
+    if (root == nullptr) return;
+    cout << root->val << " ";
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
+}
+
+class Solution {
+public:
+    vector<int> inorder;
+    void dfs(TreeNode* node) {
+        if (node == nullptr) return;
+        dfs(node->left);
+        inorder.push_back(node->val);
+        dfs(node->right);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        dfs(root);
+        return inorder[k-1];
+    }
+};
+
+int main(void) {
+    int n, k;
+    cin >> n >> k;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) cin >> nums[i];
+    TreeNode *root = buildTree(nums);
+    Solution sol;
+    cout << sol.kthSmallest(root, k) << endl;
+    return 0;
+}
--- README.md
+++ README.md
@@ -176,6 +176,7 @@ The problems are organized into directories based on the main technique or data 
 | 173 | [Binary Search Tree Iterator](https://leetcode.com/problems/binary-search-tree-iterator/description/) | [C++](./Binary%20Tree/173.%20Binary%20Search%20Tree%20Iterator/main.cpp) | | Medium | 2024/07/11 | [x] | [ ] |
 | 236 | [Lowest Common Ancestor of a Binary Tree](https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/) | [C++](./Binary%20Tree/236.%20Lowest%20Common%20Ancestor%20of%20a%20Binary%20Tree/main.cpp) | | Medium | 2024/07/11 | [x] | [ ] |
 | 530 | [Minimum Absolute Difference in BST](https://leetcode.com/problems/minimum-absolute-difference-in-bst/description/) | [C++](./Binary%20Tree/530.%20Minimum%20Absolute%20Difference%20in%20BST/main.cpp) | | Easy | 2024/07/11 | [x] | [ ] |
+| 230 | [Kth Smallest Element in a BST](https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/) | [C++](./Binary%20Tree/230.%20Kth%20Smallest%20Element%20in%20a%20BST/main.cpp) | | Medium | 2024/07/11 | [x] | [ ] |
 
 ## Tree
 
